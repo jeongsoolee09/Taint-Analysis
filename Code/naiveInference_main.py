5f060da1154ddea09c6dee109255f1895232a720
volcano0909 <volcano0909@gmail.com>
2020-01-08 14:56:46 +0900

    deleted unused function

--- Code/naiveInference_main.py
+++ Code/naiveInference_main.py
@@ -44,18 +44,4 @@ factList.to_csv("raw_data.csv", mode='w', header=False, index=False)
 
 # TODO: remove rows containing <init> and <clinit>
 
-def scoring_function(info1, info2):
-    score = 0
-    if info1[0] == info2[0]: # The two methods belong to the same package
-        score += 10
-    if (info1[2] in info2[2]) or (info2[2] in info1[2]) or (info1[2][0:2] == info2[2][0:2]) or (info1[2][0:3] == info2[2][0:3]): # The two methods start with a same prefix
-        score += 1
-        0
-    if info1[1] == info2[1]: # The two methods have a similar return type 
-        score += 10
-    if info1[3] == info2[3]: # The two methods have a same input type
-        score += 10
-    return score
-
-
 print("time :", time.time() - start)

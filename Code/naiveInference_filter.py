daedfaa0fd81e4e56f544cebb45a40d3ee013fc2
volcano0909 <volcano0909@gmail.com>
2020-01-08 15:20:59 +0900

    restrict to 1K random methods

--- Code/naiveInference_filter.py
+++ Code/naiveInference_filter.py
@@ -2,25 +2,38 @@ import pandas as pd
 import time
 from re import match
 
-methodInfo1 = pd.read_csv("raw_data.csv", index_col=0, chunksize=1000)
-methodInfo2 = pd.read_csv("raw_data.csv", index_col=0, chunksize=1000)
+start = time.time()
+
+methodInfo1 = pd.read_csv("raw_data.csv", index_col=0)
+methodInfo2 = pd.read_csv("raw_data.csv", index_col=0)
 
 # print("")
 # for i in range(5):
-#     print(methodInfo.loc[i])
-# print(methodInfo1)
-# print(methodInfo2)
+    # print(methodInfo1.loc[i])
 
 def scoring_function(info1, info2):
     score = 0
     if info1[0] == info2[0]: # The two methods belong to the same package
         score += 10
     if (info1[2] in info2[2]) or (info2[2] in info1[2]) or (info1[2][0:2] == info2[2][0:2]) or (info1[2][0:3] == info2[2][0:3]): # The two methods start with a same prefix
-        score += 1
-        0
+        score += 10
     if info1[1] == info2[1]: # The two methods have a similar return type 
         score += 10
     if info1[3] == info2[3]: # The two methods have a same input type
         score += 10
     return score
 
+edges = []
+
+print("starting bottleneck")
+for row1 in methodInfo1.itertuples(index=False):
+    for row2 in methodInfo2.itertuples(index=False):
+        if scoring_function(row1, row2) >= 20:
+            edges.append((row1,row2))
+
+print(len(list(methodInfo1['pkg'].unique()))) # outputs unique values of 'pkg' column
+
+edges = pd.DataFrame(edges, columns=["edge1", "edge2"])
+edges.to_csv("edges.csv", mode='w')
+
+print("elapsed time :", time.time() - start)

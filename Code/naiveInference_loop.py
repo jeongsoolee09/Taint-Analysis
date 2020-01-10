d4bb1fdbf7b99ed9ecaf2b8ed680017c626bd9bf
volcano0909 <volcano0909@gmail.com>
2020-01-10 12:44:00 +0900

    minor commit baby

--- Code/naiveInference_loop.py
+++ Code/naiveInference_loop.py
@@ -5,12 +5,7 @@ from re import match
 
 methods = pd.read_csv("raw_data.csv", index_col=0)
 edges = pd.read_csv("edges.csv", index_col=0)
-
-# 1. 다음을 20번 반복한다:
-# 2.     사용자에게 아무 메소드나 뽑아서 물어본다: "저기요, 이거 레이블이 뭐에요?"
-# 3.     사용자가 L (어떤 레이블)이라고 대답한다.
-# 4.     사용자의 대답을 듣고, 엣지로 연결된 모든 메소드들의 L 확률을 전부 베이즈규칙을 활용해 업데이트한다.
-
+edges.columns = pd.MultiIndex.from_product([['edge1', 'edge2'], methods.columns])
 max_index_plus_one = methods.shape[0]
 
 # Prior belief를 나타내는 자료구조 만들기
@@ -21,9 +16,8 @@ priors = priors.fillna(0.25) # Flat priors!
 def loop(times):
     for time in range(times):
         i = random.randint(0, max_index_plus_one-1)
-        query = methods.loc[i][2]
-        if time == 0:  # for debugging purposes.
-            oracle_response = input("What label does <" + query + "> bear? [src/sin/san/non]: ")
+        query = methods.loc[i][3]
+        oracle_response = input("What label does <" + query + "> bear? [src/sin/san/non]: ")
         if oracle_response == 'src':
             bayesian_update(i, priors.loc[i], 'src')
         elif oracle_response == 'sin':
@@ -56,11 +50,28 @@ def bayesian_update(method_index, prior, oracle_response):
         prior[2] = 0 * prior[2] / 0.25     # san
         prior[3] = 1 * prior[3] / 0.25     # non
     belief_propagation(method_index)
-        
 
-def belief_propagation(node):
-    """do it recursively"""
 
-    # belief_propagation()
-    
-loop(20)
+def search_in_edge(node_index):
+    # CASE 1: Match in LHS
+    neighbors_l = edges.loc[methods["name"][node_index] == edges[('edge1', 'name')]]["edge2"]
+
+    # CASE 2: Match in RHS
+    neighbors_r = edges.loc[methods["name"][node_index] == edges[('edge2', 'name')]]["edge1"]
+
+    neighbors = pd.concat([neighbors_l, neighbors_r])
+    neighbors = neighbors.drop_duplicates()
+    # # neighbors = neighbors.reset_index()
+    # # neighbors = neighbors.drop("index", 1)
+    return neighbors
+
+
+def belief_propagation(node_index):
+    """do it recursively: search for all associated tuples and call this function on those, too"""
+    neighbors = search_in_edge(node_index)
+    # print(neighbors)
+    for neighbor in neighbors.itertuples(index=False):
+        belief_propagation(int(neighbor[0]))
+
+
+belief_propagation(130)

6946dd110b5c4e3ccbe009426cd91a344e418b64
volcano0909 <volcano0909@gmail.com>
2020-01-10 15:37:49 +0900

    added desymmetrize(); WARNING: THIS IMPLEMENTATION IS INEFFICIENT

--- Code/naiveInference_filter.py
+++ Code/naiveInference_filter.py
@@ -7,10 +7,6 @@ start = time.time()
 methodInfo1 = pd.read_csv("raw_data.csv", index_col=0)
 methodInfo2 = pd.read_csv("raw_data.csv", index_col=0)
 
-# print("")
-# for i in range(5):
-    # print(methodInfo1.loc[i])
-
 def scoring_function(info1, info2):
     score = 0
     if info1[1] == info2[1]: # The two methods belong to the same package
@@ -26,6 +22,16 @@ def scoring_function(info1, info2):
 edge1 = []
 edge2 = []
 
+def desymmetrize(): # really really expensive
+    global edge1, edge2
+    tmplst = list(zip(edge1,edge2))
+    for (i,j) in tmplst:
+        if (j,i) in tmplst:
+            tmplst.remove((j,i))
+    edge1 = list(list(zip(*tmplst))[0])
+    edge2 = list(list(zip(*tmplst))[1])
+
+
 print("starting bottleneck")
 for row1 in methodInfo1.itertuples(index=False):
     for row2 in methodInfo2.itertuples(index=False):
@@ -34,15 +40,13 @@ for row1 in methodInfo1.itertuples(index=False):
             edge2.append(row2)
 print("completed bottleneck")
 
-# print(len(list(methodInfo1['pkg'].unique()))) # outputs unique values of 'pkg' column
+desymmetrize()
 
-# edges = pd.DataFrame({"edge1":edge1, "edge2":edge2})
 edge1 = pd.DataFrame(edge1, columns = methodInfo1.columns)
 edge2 = pd.DataFrame(edge2, columns = methodInfo2.columns)
-# print(edge1)
-# print(edge2)
 edges = pd.merge(edge1, edge2, left_index=True, right_index=True)
 edges.columns = pd.MultiIndex.from_product([['edge1', 'edge2'], methodInfo1.columns])
+
 edges.to_csv("edges.csv", mode='w')
 
 print("elapsed time :", time.time() - start)

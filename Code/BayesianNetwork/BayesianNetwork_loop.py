b7c27ffcf939aa1eb1913752830203654c3ccb13
volcano0909 <volcano0909@gmail.com>
2020-02-01 15:10:35 +0900

    이니셜이라우 오늘 시작이 좋다우

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -1,6 +1,22 @@
-import pomegranate as pg
+from pomegranate import *
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set_style('whitegrid')
 import time
 import random
+import numpy as np
+import csv
 
-methods = pd.read_csv("raw_data.csv", index_col=0)
+# methods : raw_data.csv
+# edges : edges.csv
+
+# 그래프에 싸이클이 없는 것을 확인함. ==> Bayesian Network로 변환 가능
+
+numpy.random.seed(0)
+numpy.set_printoptions(suppress=True)
+
+with open("raw_data.csv", "r+") as raw_data:
+    dataReader = csv.reader(raw_data)
+
+with open("edges.csv", "r+") as edges:
+    edgeReader = csv.reader(edges)
 

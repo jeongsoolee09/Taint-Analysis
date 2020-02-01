84747e7963da57c96a995253345527baa637a2b8
volcano0909 <volcano0909@gmail.com>
2020-02-01 19:07:20 +0900

    파이프라인 만드는중

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -6,19 +6,16 @@ import random
 import numpy as np
 import pandas as pd
 import csv
+import networkx as nx
 
 start = time.time()
-
-# methods : raw_data.csv
-# edges : edges.csv
 
 # 왜 하는지는 모르겠다만
 numpy.random.seed(0)
 numpy.set_printoptions(suppress=True)
 
-# csv reader 시리즈가 있어서 당장은 필요 없음
-# methods = pd.read_csv("raw_data.csv", index_col=0)
-# edges = pd.read_csv("edges.csv", index_col=0)
+edges = pd.read_csv("edges.csv", index_col=0, header=[0,1])
+edge_targets = edges.iloc[:, edges.columns.get_level_values(1)=="name"]
 
 raw_data = open("raw_data.csv", "r+")
 dataReader = csv.reader(raw_data)
@@ -26,13 +23,24 @@ edges_data = open("edges.csv", "r+")
 edgesReader = csv.reader(edges_data)
 
 # 메소드 이름 겹치는 것 (e.g. 메소드 오버라이딩)은 당장은 상관 안할 것.
-def submit_distribution_to_pom():
-    next(dataReader) # 헤더 맛없어 퉤
+def process_nodes():
+    """creates a graph for identifying root nodes"""
+    G = nx.Graph()
     for data in dataReader:
-        # code = compile(data[4] + " = DiscreteDistribution({\'src\':0.25, \'sin\':0.25, \'san\':0.25, \'non\':0.25})", "<string>", "exec")
-        code = data[4] + " = DiscreteDistribution({\'src\':0.25, \'sin\':0.25, \'san\':0.25, \'non\':0.25})"
-        exec(code, globals())
+        if data[4] == "name":
+            continue
+        code = "G.add_node('"+data[4]+"')"
+        exec(code, globals(), locals())
+    return G
 
-submit_distribution_to_pom()
+def itertuplesGenerator():
+    yield from edge_targets.itertuples(index=False):
+
+def generateEdgeTuples():
+    for edge in edge_targets.itertuples(index=False):
+        yield () + (edge[0],) + (edge[1],)
+
+process_nodes()
+
 
 print("elapsed time :", time.time() - start)

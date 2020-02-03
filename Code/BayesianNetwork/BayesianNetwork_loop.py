fa135432cd4c8f13038c5e5791d05b530c2a8272
volcano0909 <volcano0909@gmail.com>
2020-02-03 16:11:07 +0900

    아이고 힘들다

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -1,12 +1,11 @@
 from pomegranate import *
-import matplotlib.pyplot as plt
-import seaborn; seaborn.set_style('whitegrid')
 import time
 import random
 import numpy as np
 import pandas as pd
 import csv
 import networkx as nx
+from itertools import product
 
 start = time.time()
 
@@ -20,6 +19,7 @@ edgesReader = csv.reader(edges_data)
 
 flatPrior = DiscreteDistribution({'src':0.25, 'sin':0.25, 'san':0.25, 'non':0.25})
 
+# Methods for Graphs ================================
 def addNodeToGraph(G):
     """creates a graph for identifying root nodes"""
     next(dataReader) # 헤더 맛없어 퉤
@@ -27,8 +27,13 @@ def addNodeToGraph(G):
         code = "G.add_node('"+data[6]+"')"
         exec(code, globals(), locals())
 
+def findRoot(G):
+    roots = []
+    for node in G.nodes:
+        if G.in_degree(node) == 0:
+            roots.append(node)
+    return roots
 
-# 엣지 연결하기: ID와 ID를 연결
 def addEdgeToGraph(G):
     """adds edges to reference graph G"""
     next(edgesReader) # 헤더 맛없어 퉤
@@ -41,38 +46,54 @@ def addEdgeToGraph(G):
         code = "G.add_edge(firstNodeID, secondNodeID)"
         exec(code, globals(), locals())
 
-
+def initGraph():
+    """Initialize a (directed acyclic) graph for reference."""
+    G = nx.DiGraph()
+    addNodeToGraph(G)
+    addEdgeToGraph(G)
+    return G
+# ====================================================
+# Methods for BNs ====================================
 def createRootsForBN(G, BN):
     """identifies roots nodes from G and adds them to BN"""
     counter = 0
     for root in findRoot(G):
         code1 = "s"+str(counter)+" = Node(flatPrior, name=\""+root+"\")"
-        print(code1)
         exec(code1, globals(), locals())
         code2 = "BN.add_state("+"s"+str(counter)+")"
         exec(code2, globals(), locals())
         counter += 1
 
+def iterateList(lst, n):
+    """Utility function for creating a list of identical lists n times"""
+    out = []
+    for i in range(n):
+        out.append(lst)
+    return out
 
-def findRoot(G):
-    roots = []
-    for node in G.nodes:
-        if G.in_degree(node) == 0:
-            roots.append(node)
-    return roots
-            
+def createInternalLeavesForBN(G, BN):
+    root = set(findRoot(G))
+    internalLeaves = set(G.nodes)-root
+    labels = ["src", "sin", "san", "non"]
+    for node in list(internalLeaves):
+        # find the number of predecessors for each
+        condProbTableWidth = len(list(G.predecessors(node)))
+        condProbTable = iterateList(labels, condProbTableWidth)
+        
 
-def initGraph():
-    G = nx.DiGraph()
-    addNodeToGraph(G)
-    addEdgeToGraph(G)
-    return G
-    
 
+def addEdgeToBN(BN):
+    pass
+
+def initBN():
+    BN = BayesianNetwork("Automatic Inference of Method Specifications")
+    createRootsForBN(graphForReference, BN)
+    # createInternalLeavesForBN(BN) # --> 여기서 ConditionalProbabilityTable이 필요
+    # addEdgeToBN(BN)
+    return BN
+# ====================================================
 
 graphForReference = initGraph()
-BN = BayesianNetwork("Automatic Inference of Method Specifications")
-
-createRootsForBN(graphForReference, BN)
+BaysianNetwork = initBN()
 
 print("elapsed time :", time.time() - start)

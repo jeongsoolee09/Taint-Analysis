be27fef6958bbcb675f3f0db87c208483b561007
volcano0909 <volcano0909@gmail.com>
2020-02-03 13:24:58 +0900

    엣지만들기 다함

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -30,24 +30,34 @@ def process_nodes():
     return G
 
 
-def itertuplesGenerator():
-    yield from edge_targets.itertuples(index=False)
+# def itertuplesGenerator():
+#     yield from edge_targets.itertuples(index=False)
 
 
 def generateEdgeTuples():
     for edge in edge_targets.itertuples(index=False):
         yield () + (edge[0],) + (edge[1],)
 
+
 G = process_nodes()
 
-# 엣지 연결하기: ID와 ID를 연결
-# ID 만들기: <패키지: 리턴타입 이름(인풋타입)> 여기서 인풋타입==void인 경우 그냥 ()
-def addEdge():
-    next(edgesReader)
-    next(edgesReader)
-    for edge in edgesReader:
-        firstNodeID = "<"+edge[2]+": "+edge[3]+" "+edge[4]+"()" if edge[5]=="void" else "("+edge[5]+")"+">"
-        print(firstNodeID)
 
+# 엣지 연결하기: ID와 ID를 연결
+def addEdge():
+    """adds edges to graph G"""
+    global G
+    next(edgesReader) # 헤더 맛없어 퉤
+    next(edgesReader) # 헤더 맛없어 퉤
+    for row in edgesReader:
+        intype1 = "()" if row[5]=="void" else "("+row[5]+")"
+        intype2 = "()" if row[10]=="void" else "("+row[10]+")"
+        firstNodeID = "<"+row[2]+": "+row[3]+" "+row[4] + intype1 + ">"
+        secondNodeID = "<"+row[7]+": "+row[8]+" "+row[9] + intype2 + ">"
+        code = "G.add_edge(firstNodeID, secondNodeID)"
+        exec(code, globals(), locals())
+
+
+    
+addEdge()
 
 print("elapsed time :", time.time() - start)

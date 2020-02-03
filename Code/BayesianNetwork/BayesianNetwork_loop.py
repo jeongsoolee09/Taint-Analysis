c033be5c2efa4cb1475517fa5bc4ece8a37105ae
volcano0909 <volcano0909@gmail.com>
2020-02-03 12:57:00 +0900

    엣지만들쟈

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -10,10 +10,6 @@ import networkx as nx
 
 start = time.time()
 
-# 왜 하는지는 모르겠다만
-numpy.random.seed(0)
-numpy.set_printoptions(suppress=True)
-
 edges = pd.read_csv("edges.csv", index_col=0, header=[0,1])
 edge_targets = edges.iloc[:, edges.columns.get_level_values(1)=="name"]
 
@@ -22,25 +18,36 @@ dataReader = csv.reader(raw_data)
 edges_data = open("edges.csv", "r+")
 edgesReader = csv.reader(edges_data)
 
-# 메소드 이름 겹치는 것 (e.g. 메소드 오버라이딩)은 당장은 상관 안할 것.
+
 def process_nodes():
     """creates a graph for identifying root nodes"""
     G = nx.Graph()
     for data in dataReader:
-        if data[4] == "name":
+        if data[6] == "name":
             continue
-        code = "G.add_node('"+data[4]+"')"
+        code = "G.add_node('"+data[6]+"')"
         exec(code, globals(), locals())
     return G
 
+
 def itertuplesGenerator():
-    yield from edge_targets.itertuples(index=False):
+    yield from edge_targets.itertuples(index=False)
+
 
 def generateEdgeTuples():
     for edge in edge_targets.itertuples(index=False):
         yield () + (edge[0],) + (edge[1],)
 
-process_nodes()
+G = process_nodes()
+
+# 엣지 연결하기: ID와 ID를 연결
+# ID 만들기: <패키지: 리턴타입 이름(인풋타입)> 여기서 인풋타입==void인 경우 그냥 ()
+def addEdge():
+    next(edgesReader)
+    next(edgesReader)
+    for edge in edgesReader:
+        firstNodeID = "<"+edge[2]+": "+edge[3]+" "+edge[4]+"()" if edge[5]=="void" else "("+edge[5]+")"+">"
+        print(firstNodeID)
 
 
 print("elapsed time :", time.time() - start)

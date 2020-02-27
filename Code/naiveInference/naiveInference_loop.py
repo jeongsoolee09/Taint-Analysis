354af8f9e6d1e3ac63f98d1ed18100b58564928c
jslee <volcano0909@gmail.com>
2020-02-27 15:51:37 +0900

    내머리로는 도저히 룹을 못없애갯다

--- Code/naiveInference/naiveInference_loop.py
+++ Code/naiveInference/naiveInference_loop.py
@@ -7,6 +7,7 @@ ids = methods["id"]
 methods = methods.drop('id', axis=1) # unleash me when you're done!
 edges = pd.read_csv("edges.csv", index_col=0)
 edges.columns = pd.MultiIndex.from_product([['edge1', 'edge2'], methods.columns])
+
 max_index_plus_one = methods.shape[0]
 
 # Prior beliefs
@@ -52,7 +53,8 @@ def bayesian_update(method_index, oracle_response):
         priors.at[method_index, 'sin'] = 0 * priors.loc[method_index, 'sin'] / 0.25     # sin
         priors.at[method_index, 'san'] = 0 * priors.loc[method_index, 'san'] / 0.25     # san
         priors.at[method_index, 'non'] = 1 * priors.loc[method_index, 'non'] / 0.25     # non
-    belief_propagation(method_index, oracle_response, 2)    # propagation depth = 2
+    belief_propagation(method_index, oracle_response, 3)
+
 
 def bayesian_update_without_propagation(method_index, oracle_response):
     global priors
@@ -80,41 +82,40 @@ def bayesian_update_without_propagation(method_index, oracle_response):
 
 def search_in_edge(node_index):
     """finds an immediate neighbor on the graph structure"""
-    # CASE 1: Match in LHS
-    neighbors_l = edges.loc[methods["name"][node_index] == edges[('edge1', 'name')]]["edge2"]
+    # cond1 = methods["index"][node_index] == edges[('edge1','index')]
+    cond2 = methods["pkg"][node_index] == edges[('edge1','pkg')]
+    cond3 = methods["rtntype"][node_index] == edges[('edge1','rtntype')]
+    cond4 = methods["name"][node_index] == edges[('edge1','name')]
+    cond5 = methods["intype"][node_index] == edges[('edge1','intype')]
 
-    # CASE 2: Match in RHS
-    neighbors_r = edges.loc[methods["name"][node_index] == edges[('edge2', 'name')]]["edge1"]
-
-    neighbors = pd.concat([neighbors_l, neighbors_r])
+    neighbors = edges[cond2 & cond3 & cond4 & cond5]["edge2"]
     neighbors = neighbors.drop_duplicates()
     return neighbors
 
 
-def belief_propagation(node_index, oracle_response, times):
+def belief_propagation(node_index, oracle_response, depth):
     """do it recursively: search for all associated tuples and call this function on those, too"""
-    print(node_index)
-    if times == 0:
+    if depth == 0:
         return
     neighbors = search_in_edge(node_index)
     for neighbor in neighbors.itertuples(index=False):
-        if neighbor[0] == "index":   # empty dataframe, i.e. no further neighbors
+        if neighbor[0] == "index":   # empty methods, i.e. no further neighbors
             return
         bayesian_update_without_propagation(int(neighbor[0]), oracle_response)
-        belief_propagation(int(neighbor[0]), oracle_response, times)
+        belief_propagation(int(neighbor[0]), oracle_response, depth-1)
 
 loop(10)
 
 def report_result():
     global priors
-    condition_src = priors['src'] == 1
-    condition_sin = priors['sin'] == 1
-    condition_san = priors['san'] == 1
-    condition_non = priors['non'] == 1
-    nonzeros = priors[condition_src | condition_sin | condition_san | condition_non]
-    print("Touched {} methods".format(nonzeros.shape[0]))
+    one_src = priors['src'] == 1
+    one_sin = priors['sin'] == 1
+    one_san = priors['san'] == 1
+    one_non = priors['non'] == 1
+    ones = priors[one_src | one_sin | one_san | one_non]
+    print("Touched {} methods".format(ones.shape[0]))
     print("Labels of the following methods are updated:")
-    print(nonzeros["id"])    
+    print(ones["id"])    
     priors.to_csv("result.csv", mode='w')
     print("report saved as result.csv")
 

9eab00406acb236fdb48e5c268996af5fc96b275
JeongSoo Lee <volcano0909@gmail.com>
2020-04-10 20:58:37 +0900

    formal 찾기 && 필터링 모두 구현

--- Code/infer/infer/src/spechunter/LiveRange.ml
+++ Code/infer/infer/src/spechunter/LiveRange.ml
@@ -1,5 +1,6 @@
 open! IStd
-open DefLocAlias
+open DefLocAliasSearches
+open DefLocAliasDomain
 
 module Hashtbl = Caml.Hashtbl
 module S = DefLocAliasDomain.AbstractState
@@ -17,15 +18,34 @@ let add_formal_args (key:Procname.t) (value:Var.t list) = Hashtbl.add formal_arg
 
 let get_formal_args (key:Procname.t) = Hashtbl.find formal_args key
 
-let rec batch_add_to_formals (key:Procname.t) (values:Var.t list list)  =
-  match values with
-  | [] -> ()
-  | h::t -> add_formal_args key h; batch_add_to_formals key t
+let summary_table = DefLocAlias.TransferFunctions.summaries
+
+let update_summary (key:Procname.t) (astate:S.t) = Hashtbl.replace summary_table key astate
+
+let get_summary (key:Procname.t) = Hashtbl.find summary_table key
 
 (** collect all formals from a summary *)
-let collect_formals summary =
+(* uses the invariant that procnames are unique in a state *)
+let collect_formals (summary:S.t) =
   let astates = S.elements summary in
-  raise NotImplemented
-  
-(** filter all formals from a summary *)
-(* let filter_formals *)
+  let procname = first_of @@ List.nth_exn astates 0 in
+  let locations = List.sort ~compare:Location.compare (List.map ~f:third_of astates) in (* 잘 되겠지? 안 되면 라인 넘버를 끌고 오자 *)
+  let earliest_location = List.nth_exn locations 0 in
+  let parameters_withthis = List.map ~f:second_of @@ search_tuples_by_loc earliest_location astates in
+  let parameters = List.filter ~f:(Var.is_this >> not) parameters_withthis in
+  add_formal_args procname parameters
+
+
+(** filter all formals from a summary, accessed by a procname *)
+let filter_formals (methname:Procname.t) =
+  let formals = get_summary methname in
+  let astates = S.elements formals in
+  let locations = List.sort ~compare:Location.compare (List.map ~f:third_of astates) in (* 잘 되겠지? 안 되면 라인 넘버를 끌고 오자 *)
+  let earliest_location = List.nth_exn locations 0 in
+  let tuples_withthis = search_tuples_by_loc earliest_location astates in
+  let formaltuples = List.filter ~f:(fun tup -> not @@ Var.is_this (second_of tup)) tuples_withthis in
+  S.diff formals (S.of_list formaltuples)
+
+
+(** interface with the driver *)
+let run_lrm summary_table = raise NotImplemented 

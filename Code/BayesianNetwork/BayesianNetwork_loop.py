ee179a3b9f375c369b3bdb1defdd121742ad6d5c
JeongSoo Lee <volcano0909@gmail.com>
2020-04-14 20:31:58 +0900

    스타일2

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -14,23 +14,27 @@ import itertools as it
 print("starting..")
 start = time.time()
 
-edges = pd.read_csv("edges.csv", index_col=0, header=[0,1])
-edge_targets = edges.iloc[:, edges.columns.get_level_values(1)=="name"]
+edges = pd.read_csv("edges.csv", index_col=0, header=[0, 1])
+edge_targets = edges.iloc[:, edges.columns.get_level_values(1) == "name"]
 
 raw_data = open("raw_data.csv", "r+")
 dataReader = csv.reader(raw_data)
 edges_data = open("edges.csv", "r+")
 edgesReader = csv.reader(edges_data)
 
-flatPrior = DiscreteDistribution({'src':0.25, 'sin':0.25, 'san':0.25, 'non':0.25})
+flatPrior = DiscreteDistribution({'src': 0.25, 'sin': 0.25, 'san': 0.25, 'non': 0.25})
+
 
 # Methods for Graphs ================================
+
+
 def addNodeToGraph(G):
     """creates a graph for identifying root nodes"""
-    next(dataReader) # Headers don't taste good
+    next(dataReader)  # Headers don't taste good
     for data in dataReader:
         code = "G.add_node('"+data[6]+"')"
         exec(code, globals(), locals())
+
 
 def findRoot(G):
     roots = []
@@ -39,17 +43,19 @@ def findRoot(G):
             roots.append(node)
     return roots
 
+
 def addEdgeToGraph(G):
     """adds edges to reference graph G"""
     next(edgesReader)
     next(edgesReader)
     for row in edgesReader:
-        intype1 = "()" if row[5]=="void" else "("+row[5]+")"
-        intype2 = "()" if row[10]=="void" else "("+row[10]+")"
+        intype1 = "()" if row[5] == "void" else "("+row[5]+")"
+        intype2 = "()" if row[10] == "void" else "("+row[10]+")"
         firstNodeID = "<"+row[2]+": "+row[3]+" "+row[4] + intype1 + ">"
         secondNodeID = "<"+row[7]+": "+row[8]+" "+row[9] + intype2 + ">"
         code = "G.add_edge(firstNodeID, secondNodeID)"
         exec(code, globals(), locals())
+
 
 def initGraph():
     """Initialize a (directed acyclic) graph for reference."""
@@ -57,8 +63,12 @@ def initGraph():
     addNodeToGraph(G)
     addEdgeToGraph(G)
     return G
+
+
 # ====================================================
 # Methods for BNs ====================================
+
+
 def createRootsForBN(G, BN):
     """identifies roots nodes from G and adds them to BN"""
     counter = 0
@@ -69,8 +79,6 @@ def createRootsForBN(G, BN):
         exec(code2, globals(), locals())
         counter += 1
 
-def toListOfList(lst):
-    return list(map(lambda x: list(x), lst))
 
 def createInternalLeavesForBN(G, BN):
     root = set(findRoot(G))
@@ -82,11 +90,13 @@ def createInternalLeavesForBN(G, BN):
         condProbTable = list(condProbTableGen)
         condProbTable = it.product(*condProbTable)
         condProbTable = it.chain.from_iterable(condProbTable)
-        temp = np.fromiter(condProbTable, int).reshape(-1,condProbTableWidth)
+        temp = np.fromiter(condProbTable, int).reshape(-1, condProbTableWidth)
     # condProbTable = ConditionalProbabilityTable(condProbTable, G.predecessors(node))
+
 
 def addEdgeToBN(BN):
     pass
+
 
 def initBN():
     global graphForReference
@@ -95,7 +105,10 @@ def initBN():
     createInternalLeavesForBN(graphForReference, BN)
     # addEdgeToBN(BN)
     return BN
+
+
 # ====================================================
+
 
 graphForReference = initGraph()
 # BaysianNetwork = initBN()

2b6d23b6b361bd2ab8e385ee6c8f3dea09f43e43
JeongSoo Lee <volcano0909@gmail.com>
2020-04-27 10:42:47 +0900

    밀기 전 커밋

--- Code/naiveInference/naiveInference_loop.py
+++ Code/naiveInference/naiveInference_loop.py
@@ -133,7 +133,6 @@ def update_node_color():
             label = labeldict[probs.index(max(probs[1], probs[2],
                                               probs[3], probs[4]))]
             color = colordict[label]
-        
             print(nodelist_index)
             vis_color_map[nodelist_index] = color
 

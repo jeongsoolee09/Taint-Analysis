1833b1cfbcaa3f0d8cd04248f4a1247fff2c7917
jslee <volcano0909@gmail.com>
2020-04-28 14:14:04 +0900

    벤치마크 수정

--- Code/benchmarks/fabricated/WhatIWantExample.java
+++ Code/benchmarks/fabricated/WhatIWantExample.java
@@ -30,7 +30,7 @@ public class WhatIWantExample {
         m3(w);
     }
 
-    public static void main(String args[]) {
+    public static void main() {
         WhatIWantExample w = new WhatIWantExample();
         w.f();
     }

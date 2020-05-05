5d7285a06ec13c344cfceec6ed1b57cc12d9344e
jslee <volcano0909@gmail.com>
2020-05-05 21:17:41 +0900

    엣지 타입 고려

--- Code/BayesianNetwork/BayesianNetwork_loop.py
+++ Code/BayesianNetwork/BayesianNetwork_loop.py
@@ -57,25 +57,6 @@ def create_var_and_chain():
     return var_and_chain
 
 
-# should be called AFTER graph_for_reference is constructed
-def create_tactics(chain_without_var):
-    """consuming a chain in tuple list form,
-    plans ahead how to question the oracle"""
-    setofallmethods = graph_for_reference.nodes()
-    src_suspects = collect_src(chain_without_var)  # priority 4
-    san_suspects = collect_san(chain_without_var)  # priority 3
-    sin_suspects = collect_sin(chain_without_var)  # priority 2
-    non_suspects = collect_non(chain_without_var, src_suspects, san_suspects,
-                               sin_suspects, setofallmethods)  # priority 1
-    return {"src": src_suspects, "san": san_suspects,
-            "sin": sin_suspects, "non": non_suspects}
-
-
-var_and_chain = create_var_and_chain()
-tactics_per_var = list(map(lambda x: (x[0], create_tactics(x[1])),
-                           var_and_chain))
-
-
 def collect_src(chain_without_var):  # priority 4
     out = []
     chain_head = chain_without_var[0]
@@ -117,6 +98,28 @@ flatPrior = DiscreteDistribution({'src': 0.25, 'sin': 0.25,
                                   'san': 0.25, 'non': 0.25})
 
 
+def df_reader():
+    with open("df.txt", "r+") as df:
+        lines = df.readlines()
+        lines = list(map(lambda line: line.rstrip(), lines))
+        lines = list(map(lambda line: line.split(", "), lines))
+        lines = list(map(lambda line: tuple(line), lines))
+    return lines
+
+
+def call_reader():
+    with open("callg.txt", "r+") as callg:
+        lines = callg.readlines()
+        lines = list(map(lambda line: line.rstrip(), lines))
+        lines = list(map(lambda line: line.split(", "), lines))
+        lines = list(map(lambda line: tuple(line), lines))
+    return lines
+
+
+df_edges = df_reader()
+call_edges = call_reader()
+
+
 # Methods for Graphs ================================
 
 
@@ -154,6 +157,30 @@ def add_edge_to_graph(G):
         G.add_edge(firstNodeID, secondNodeID)
 
 
+def find_edges_of(node):
+    global graph_for_reference
+    lookup_edges = graph_for_reference.edges
+    out = []
+    for (m1, m2) in lookup_edges:
+        if m2 == node:
+            out.append((m1, m2))
+    return out
+
+
+def decide_edgekind(node):
+    """하나의 node를 받아 그 node가 속한 모든 엣지의 각 종류(df, call, sim)를 판별한다."""
+    edges = find_edges_of(node)
+    out = []
+    for edge in edges:
+        if edge in df_edges:
+            out.append((edge, "df"))
+        elif edge in call_edges:
+            out.append((edge, "call"))
+        else:
+            out.append((edge, "sim"))
+    return out
+
+
 def init_graph():
     """Initialize a (directed acyclic) graph for reference."""
     G = nx.DiGraph()
@@ -182,6 +209,9 @@ def create_internal_leaves_for_BN(G, BN):
     internal_leaves = set(G.nodes)-root
     labels = [1, 2, 3, 4]       # src, sin, san, non
     for node in internal_leaves:
+        print(node)
+        edgekinds = decide_edgekind(node)
+        print(edgekinds)
         cond_prob_table_width = len(list(G.predecessors(node)))
         cond_prob_table_gen = it.repeat(labels, cond_prob_table_width+1)
         cond_prob_table = list(cond_prob_table_gen)
@@ -189,7 +219,6 @@ def create_internal_leaves_for_BN(G, BN):
         cond_prob_table = it.chain.from_iterable(cond_prob_table)
         temp = np.fromiter(cond_prob_table, int).reshape(-1, cond_prob_table_width+1)
         print(temp)
-        print(type(temp))
     # cond_prob_table = ConditionalProbabilityTable(cond_prob_table, G.predecessors(node))
 
 
@@ -207,10 +236,28 @@ def init_BN():
 
 
 # ====================================================
-
-
 graph_for_reference = init_graph()
 BN_for_inference = init_BN()
+
+
+# should be called AFTER graph_for_reference is constructed
+def create_tactics(chain_without_var):
+    """consuming a chain in tuple list form,
+    plans ahead how to question the oracle"""
+    setofallmethods = graph_for_reference.nodes()
+    src_suspects = collect_src(chain_without_var)  # priority 4
+    san_suspects = collect_san(chain_without_var)  # priority 3
+    sin_suspects = collect_sin(chain_without_var)  # priority 2
+    non_suspects = collect_non(chain_without_var, src_suspects, san_suspects,
+                               sin_suspects, setofallmethods)  # priority 1
+    return {"src": src_suspects, "san": san_suspects,
+            "sin": sin_suspects, "non": non_suspects}
+
+
+var_and_chain = create_var_and_chain()
+tactics_per_var = list(map(lambda x: (x[0], create_tactics(x[1])),
+                           var_and_chain))
+
 
 print("# of nodes: ", len(list(graph_for_reference.nodes())))
 print("# of edges: ", len(list(graph_for_reference.edges())))

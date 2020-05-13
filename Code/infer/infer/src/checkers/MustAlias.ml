541df236a6539bab1cc0adbfc87322e4ec81fe7f
jslee <volcano0909@gmail.com>
2020-05-13 15:56:56 +0900

    함수 프로토타입 몇 개 추가

--- Code/infer/infer/src/checkers/MustAlias.ml
+++ Code/infer/infer/src/checkers/MustAlias.ml
@@ -15,9 +15,45 @@ module E = struct
   type t = Fieldname.t [@@deriving compare]
   let default =
     let dummy_java_classname = JavaClassName.from_string "DummyClass" in
-    let typ_name = Fieldname.make (JavaClass dummy_java_classname) "dummy" in
-    typ_name
+    Fieldname.make (JavaClass dummy_java_classname) "dummy"
 end
 
 module G = Graph.Imperative.Digraph.ConcreteBidirectionalLabeled (V) (E)
 
+
+(** mapping from procname to graph *)
+module GlobalInfo = PrettyPrintable.MakePPMap (Procname)  
+
+
+let global_info = ref GlobalInfo.empty
+
+
+let update_global_info (key:Procname.t) (value:G.t) =
+  global_info := GlobalInfo.add key value !global_info
+
+
+(** 대상 SourceFile에 들어 있는 Set of all method를 계산한다. *)
+let calculate () =
+  let all_source_files = SourceFiles.get_all ~filter:(fun _ -> true) () in
+  let all_procnames_listlist = List.map ~f:SourceFiles.proc_names_of_source all_source_files in
+  (* 아직은 파일이 하나밖에 없으니까 *)
+  List.concat all_procnames_listlist
+  
+
+(** init the graph for every procnames: read the set of all methods *)
+let batch_initialize_graph () =
+  let set_of_all_methods = calculate () in
+  List.iter set_of_all_methods ~f:(fun meth -> update_global_info meth (G.create ()))
+
+
+(** updates the graph based on instruction types *)
+let update_graph (instr:Sil.instr) graph = raise NotImplemented
+
+(** implements all_aliases *)
+let all_aliases ap = raise NotImplemented
+
+(** implements intersect *)
+let intersect g1 g2 = raise NotImplemented
+
+(** implements gc *)
+let gc g = raise NotImplemented

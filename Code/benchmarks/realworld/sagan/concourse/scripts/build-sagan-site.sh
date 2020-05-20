34c899d7d25f4962f10b570283a6116009072526
JeongSoo Lee <volcano0909@gmail.com>
2020-05-20 11:28:56 +0900

    removed sagan

--- Code/benchmarks/realworld/sagan/concourse/scripts/build-sagan-site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e -u -x
-source $(dirname $0)/common.sh
-chown gradle:gradle -R ./sagan-site-repo
-pushd sagan-site-repo > /dev/null
-su gradle -c "./gradlew --console plain :sagan-site:build"
-popd > /dev/null
-cp -R sagan-site-repo/sagan-site/build/* build
--- Code/benchmarks/realworld/sagan/sagan-site/src/main/java/sagan/support/nav/Section.java
+++
@@ -1,9 +0,0 @@
-package sagan.support.nav;
-
-/**
- * Used in conjunction with {@link sagan.support.nav.Navigation} to indicate which section
- * of the site-wide navigation is currently active and highlighted.
- */
-public enum Section {
-    DOCS, GUIDES, PROJECTS, BLOG
-}

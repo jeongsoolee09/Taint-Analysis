edc0a89b2f940399d10ce6e22f0ed0f48d57eb3f
jslee <volcano0909@gmail.com>
2020-07-06 17:16:05 +0900

    쉽게 가즈아

---
+++ Code/BayesianNetwork/BayesianNetwork_BasicCPT.py
@@ -0,0 +1,9 @@
+from z3 import *
+from functools import reduce
+
+"""너무 모든 것을 Z3에 맡기려다 보니 문제 풀기가 어려워졌다. 특히, 어떤 조건 하에서 cell이 어떤 magnitude를 가지는지를 Z3로 풀려다 보니 특히 어려워진 것. 그 부분을 순정 파이썬으로 메꾸려 한다. 쉽게 가즈아"""
+
+
+# Core function
+def make_basic_CPT():
+    pass

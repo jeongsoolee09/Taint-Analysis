6d43df68ac521af02f7232b189de374f519373d0
jslee <volcano0909@gmail.com>
2020-07-10 19:27:08 +0900

    make_call_df_cpt 완성

--- Code/BayesianNetwork/make_CPT.py
+++ Code/BayesianNetwork/make_CPT.py
@@ -1,15 +1,9 @@
 from z3 import *
 from functools import reduce
 import numpy as np
+import pandas as pd
 
-"""너무 모든 것을 Z3에 맡기려다 보니 문제 풀기가 어려워졌다. 특히, 어떤 조건 하에서 cell이 어떤 magnitude를 가지는지를 Z3로 풀려다 보니 특히 어려워진 것. 그 부분을 순정 파이썬+numpy로 메꾸려 한다. 쉽게 가즈아"""
-
-MAGNITUDE = ["YG", "Y", "G", "W"]
-LABELDICT = {1: "src", 2: "sin", 3: "san", 4: "non"}
-MAGDICT = {1: "YG", 2: "Y", 3: "G", 4: "W"}
-
-# CPT를 만들되, 개략적인 magnitude만 들어가 있게끔 만든다.
-
+cpt_lookup_table = np.loadtxt("CPT_lookup_table.csv", delimiter=',')
 
 def labels_of(row, column, N):
     """set the label of all parents and child a cell represents, given the cell's index and the total number of parents"""
@@ -20,6 +14,7 @@ def labels_of(row, column, N):
     child_label = row+1
     out.append((N+1, child_label))  # the last pair refers to the child
     return out
+
 
 # https://stackoverflow.com/questions/3844801/check-if-all-elements-in-a-list-are-identical
 def elements_are_identical(iterator):
@@ -40,7 +35,8 @@ def label_identical(row, column, N):
 
 
 # Core functions
-def make_call_sim_CPT(N):
+def make_call_sim_mag_table(N):
+    """parent가 쏜 엣지가 call/sim밖에 없을 때, magnitude table을 만든다."""
     arr = np.ones((4, 4**N)).astype(int)
     for (i,j), value in np.ndenumerate(arr):  # for efficient looping
         if label_identical(i, j, N):
@@ -50,7 +46,8 @@ def make_call_sim_CPT(N):
     return arr
 
 
-def make_df_CPT(N):
+def make_df_mag_table(N):
+    """parent가 쏜 엣지가 df밖에 없을 때, magnitude table을 만든다."""
     arr = np.ones((4, 4**N)).astype(int)
     for (i,j), value in np.ndenumerate(arr):  # for efficient looping
         parent_labels = labels_of(i, j, N)
@@ -86,16 +83,14 @@ def to_be_mutated(labels, call_sim_parents):
 
 
 
-def make_call_df_CPT(edges):
-    """df랑 call/sim이 섞여 있기 때문에 어떤 parent와 어떤 edge가 연관되어 있는가의 정보가 주어져야 함"""
+def make_call_df_mag_table(edges):
+    """parent가 쏜 엣지에 call/sim과 df가 섞여 있을 때, magnitude table을 만든다."""
     N = len(edges)
-    df_CPT = make_df_CPT(N)  # 먼저 df_CPT를 만들어 둔 다음
+    df_CPT = make_df_mag_table(N)  # 먼저 df_CPT를 만들어 둔 다음
     parents_and_edges = list(zip(range(1, N+2), edges))  # n번째 parent와 그 엣지의 쌍들
     call_sim_parents = list(filter(lambda tup: tup[1] == "call" or
                                    tup[1] == "sim", parents_and_edges))
     call_sim_parents = list(map(lambda tup: tup[0], call_sim_parents))
-    print(parents_and_edges)
-    print(call_sim_parents)
     for (i,j), value in np.ndenumerate(df_CPT):
         cell_labels = labels_of(i, j, N)
         if to_be_mutated(cell_labels, call_sim_parents):
@@ -103,3 +98,41 @@ def make_call_df_CPT(edges):
                 df_CPT[i,j] = 3
             else:  # Yellow였다면, Yellow-Green으로 바꿔 줌
                 df_CPT[i,j] = 1
+    return df_CPT
+
+
+def base_5_tup_to_decimal(tup):
+    return tup[0]*125+tup[1]*25+tup[2]*5+tup[3]
+
+
+def get_row_of_lookup_table(tup):
+    """column 값이 들어 있는 4개짜리 튜플을 받아서, lookup table의 몇 번째 행에 접근하면 되는지를 파악한다."""
+    only_labels = cpt_lookup_table[:,:4]
+    ndarray_slice = np.asarray(tup)
+    broadcasted = only_labels == ndarray_slice
+    return np.where(np.all(broadcasted, axis=1))[0][0]
+
+
+def make_call_df_CPT(edges):
+    """edge 중 call과 df가 섞여 있을 때, magnitude table로부터 CPT를 만들어 낸다."""
+    mag_table = make_call_df_mag_table(edges)
+    _, cols = mag_table.shape
+    out = []
+    for col in range(cols):
+        col_tuple = (mag_table[0][col], mag_table[1][col], mag_table[2][col], mag_table[3][col])
+        lookup_table_row = cpt_lookup_table[get_row_of_lookup_table(col_tuple)]
+        lookup_table_row = lookup_table_row[4:8]
+        out.append(lookup_table_row)
+    out = np.asarray(out)
+    return out
+        
+        
+
+def make_call_sim_CPT(N):
+    """edge에 call/sim만이 있을 때, magnitude table로부터 CPT를 만들어 낸다."""
+    pass
+
+
+def make_df_CPT(N):
+    """edge에 df만이 있을 때, magnitude table로부터 CPT를 만들어 낸다."""
+    pass

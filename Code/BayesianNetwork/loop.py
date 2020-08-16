f6721f88c489ed1203cacd9f094286a315df63d5
이정수 <jslee@ijeongsus-iMac.local>
2020-08-16 15:24:46 +0900

    heuristic 구현 실패;;

--- Code/BayesianNetwork/loop.py
+++ Code/BayesianNetwork/loop.py
@@ -361,7 +361,7 @@ def random_loop(current_asked, current_evidence, prev_snapshot, precision_list, 
         return random_loop(current_asked+[query], current_evidence, new_snapshot,  precision_list+[current_precision_list], stability_list+[current_stability_list])
 
 
-def d_connected(node, current_asked):
+def d_connected(node, current_asked, pool):
     """현재까지 물어본 노드들이 주어졌을 때, node와 조건부 독립인 노드들의 set을 찾아낸다. Complexity: O(n)."""
     out = set()
     for other_node in graph_for_reference.nodes:
@@ -390,17 +390,75 @@ def forall(unary_pred, collection):
     return reduce(lambda acc, elem: unary_pred(elem) and acc, collection, True)
 
 
-def find_max_d_con(current_asked, updated_nodes, list_of_all_node):  # 전체 node pool을 제한할 수 있음
+def non_nodes_to_skip(snapshot):
+    """현재 스냅샷으로 계산된 names_and_labels를 받아서, 다음을 만족하는 노드 n을 모은다:
+       n과 연결된 이웃 (parent 혹은 child)이
+         1. non으로 고정되었고,
+         2. 맞고 있는/쏘고 있는 화살표가 non/sim밖에 없다."""
+    can_be_excluded = []
+    for node_name in graph_for_reference.nodes:
+        parents = graph_for_reference.predecessors(node_name)
+        children = graph_for_reference.successors(node_name)
+        from_parents_edges = []
+        to_children_edges = []
+        for parent in parents:
+            from_parents_edges.append((parent, node_name))
+        for child in children:
+            to_children_edges.append((node_name, child))
+        # 이제 엣지의 레이블을 포착해야 한다.
+        if forall(lambda edge: edge not in df_edges, from_parents_edges) and\
+           forall(lambda edge: edge not in df_edges, to_children_edges):
+            can_be_excluded.append(node_name)
+    return set(can_be_excluded)
+
+
+def find_max_d_con(current_asked, updated_nodes, list_of_all_node, **kwargs):  # 전체 node pool을 제한할 수 있음
     """graph_for_reference의 node들 중에서 가장 d_connected node가 가장 많은 노드를 찾아낸다."""
     d_connected_set_dict = dict()
     for node in list_of_all_node:
-        d_connected_set_dict[node] = d_connected(node, current_asked)
+        d_connected_set_dict[node] = d_connected(node, current_asked, list_of_all_node)
     d_connected_set_dict = valmap(lambda set_: set_-set(current_asked)-set(updated_nodes), d_connected_set_dict)
     if forall(lambda set_: set_ == set(), d_connected_set_dict.values()):  # no more to ask
         return None
     d_connected_len_dict = valmap(lambda set_: len(set_), d_connected_set_dict)
-    max_key = max(d_connected_len_dict, key=lambda key: d_connected_len_dict[key])
-    return max_key, d_connected_set_dict[max_key]
+    nth = kwargs['nth']
+    # print("nth:", nth)
+    # dictionary에서 value만을 추출해 내림차순으로 정렬한다.
+    if 'prune' in kwargs.keys():
+        to_prune = kwargs['prune']
+        number_of_sets_sorted = list(set(d_connected_len_dict.values()))  # not yet sorted..
+        number_of_sets_sorted.sort(reverse=True)  # now sorted!
+        nth_largest_node_names = []
+        nth_largest_cardinality = number_of_sets_sorted[nth]
+        print("nth_largest_cardinality:", nth_largest_cardinality)
+        print("d_connected_len_dict:", d_connected_len_dict)
+
+        for node_name, cardinality in d_connected_len_dict.items():
+            if nth_largest_cardinality == cardinality and\
+               node_name not in current_asked and\
+               node_name not in to_prune:
+                nth_largest_node_names.append(node_name)
+        print("nth_largest_node_names:",  nth_largest_node_names)
+        if nth_largest_node_names == []:
+            while nth_largest_node_names == []:
+                number_of_sets_sorted.remove(number_of_sets_sorted[0])
+                nth_largest_cardinality = number_of_sets_sorted[nth]
+                print("nth_largest_cardinality:", nth_largest_cardinality)
+                for node_name, cardinality in d_connected_len_dict.items():
+                    if nth_largest_cardinality == cardinality and\
+                       node_name not in current_asked and\
+                       node_name not in to_prune:
+                        nth_largest_node_names.append(node_name)
+                if nth_largest_node_names == []:
+                    for node_name, cardinality in d_connected_len_dict.items():
+                        if nth_largest_cardinality == cardinality and\
+                           node_name not in current_asked:
+                            nth_largest_node_names.append(node_name)
+        out = nth_largest_node_names[0]
+        return out, d_connected_set_dict[out]
+    else:
+        max_key = max(d_connected_len_dict, key=lambda key: d_connected_len_dict[key])
+        return max_key, d_connected_set_dict[max_key]
 
 
 def is_confident(parameters):
@@ -427,38 +485,60 @@ def time_to_terminate(BN, current_evidence):
     return reduce(lambda acc, lst: is_confident(lst) and acc, dist_probs, True)
 
 
-def print_distrib(snapshot):
-    names_and_params = make_names_and_params(snapshot)
-    for i in names_and_params:
-        print(i)
-
-
-def tactical_loop(interaction_number, current_asked, current_evidence, updated_nodes, prev_snapshot, precision_list, stability_list, **kwargs):
-    """the main interaction functionality, asking tactically using d-separation
-       available kwargs:
-        - skip_call_sim_heur: skip an interaction for non methods connected with call/sim edges.
+def tactical_loop(interaction_number, current_asked, current_evidence, updated_nodes, prev_snapshot, precision_list, stability_list, **config):
+    """the main interaction functionality (loops via recursion), asking tactically using d-separation
+       parameters:
+            - interaction_number: number of interactions performed so far.
+            - current_asked: names of currently asked nodes.
+            - current_evidence: dict of given evidences accumulated so far
+            - updated_nodes: updated nodes which are currently being tracked of
+            - prev_snapshot: snapshot from the previous call
+            - precision_list: accumulated precision values
+            - stability_list: accumulated stability values
+       available config:
+            - skip_call_sim_heur (bool): skip an interaction for non methods connected with call/sim edges.
     """
-    # some variables to make our code resemble the English language
-    there_are_nodes_left = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes)
-    there_are_no_nodes_left = not there_are_nodes_left
-    its_time_to_terminate = time_to_terminate(BN_for_inference, current_evidence)
-    not_yet_time_to_terminate = not its_time_to_terminate
+    # some variables to make our code resemble English
+    if config["skip_call_sim_heur"]:  # now we prune the pool with non_nodes_to_skip
+        pruneable_nodes = non_nodes_to_skip(graph_for_reference.nodes)
+        # print(pruneable_nodes)
+        there_are_nodes_left = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes, prune=pruneable_nodes, nth=0)
+        there_are_nodes_left = there_are_nodes_left
+        there_are_no_nodes_left = not there_are_nodes_left
+        its_time_to_terminate = time_to_terminate(BN_for_inference, current_evidence)
+        not_yet_time_to_terminate = not its_time_to_terminate
 
-    # query, dependent_nodes = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes)
-    maybe_query_tuple = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes)
-    if maybe_query_tuple == None:
-        query = None
-        dependent_nodes = []
-    else:
-        query = maybe_query_tuple[0]
-        dependent_nodes = maybe_query_tuple[1]
+        maybe_query_tuple = there_are_nodes_left
+        if maybe_query_tuple == None:
+            query = None
+            dependent_nodes = []
+        else:
+            query = maybe_query_tuple[0]
+            dependent_nodes = maybe_query_tuple[1]
+            # i = 0
+            # while query in pruneable_nodes:
+            #     query, dependent_nodes = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes, prune=pruneable_nodes, nth=i)
+            #     i += 1
+    else:  # vanilla
+        there_are_nodes_left = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes, nth=0)
+        there_are_no_nodes_left = not there_are_nodes_left
+        its_time_to_terminate = time_to_terminate(BN_for_inference, current_evidence)
+        not_yet_time_to_terminate = not its_time_to_terminate
+
+        maybe_query_tuple = find_max_d_con(current_asked, updated_nodes, graph_for_reference.nodes, nth=0)
+        if maybe_query_tuple == None:
+            query = None
+            dependent_nodes = []
+        else:
+            query = maybe_query_tuple[0]
+            dependent_nodes = maybe_query_tuple[1]
 
     if there_are_no_nodes_left and not_yet_time_to_terminate:
         if set(graph_for_reference.nodes) == set(current_asked):
             print("\nWarning: some distributions are not fully determined.\n")
             return prev_snapshot, precision_list, stability_list
         else:
-            query, dependent_nodes = find_max_d_con([], [], remove_sublist(graph_for_reference.nodes, current_asked))
+            query, dependent_nodes = find_max_d_con([], [], remove_sublist(graph_for_reference.nodes, current_asked), nth=0)
     elif there_are_no_nodes_left and its_time_to_terminate:
         return prev_snapshot, precision_list, stability_list
     elif there_are_nodes_left and not_yet_time_to_terminate:
@@ -467,7 +547,7 @@ def tactical_loop(interaction_number, current_asked, current_evidence, updated_n
         raise ThisIsImpossible
 
     oracle_response = input("What label does <" + query + "> bear? [src/sin/san/non]: ")
-    updated_nodes = updated_nodes + list(d_connected(query, current_asked))
+    updated_nodes = updated_nodes + list(d_connected(query, current_asked, graph_for_reference.nodes))
     current_asked = current_asked + [query]
     if oracle_response == 'src':
         current_evidence[query] = 1
@@ -486,7 +566,7 @@ def tactical_loop(interaction_number, current_asked, current_evidence, updated_n
     draw_stability_graph(list(range(1, len(BN_for_inference.states)+1)), stability_list)
     visualize_snapshot(new_snapshot, dependent_nodes)
     plt.show(block=False)
-    return tactical_loop(interaction_number+1, current_asked, current_evidence, updated_nodes, new_snapshot, precision_list, stability_list)
+    return tactical_loop(interaction_number+1, current_asked, current_evidence, updated_nodes, new_snapshot, precision_list, stability_list, **config)
 
 
 def visualize_snapshot(snapshot, dependent_nodes):
@@ -518,8 +598,8 @@ def visualize_snapshot(snapshot, dependent_nodes):
     for dependent_node in dependent_nodes:
         coord_lists.append(node_posmap[dependent_node])
     coord_lists = np.asarray(coord_lists)
-    polygon = ptch.Polygon(coord_lists, closed=True, alpha=0.4)
-    ax.add_patch(polygon)
+    # polygon = ptch.Polygon(coord_lists, closed=True, alpha=0.4)
+    # ax.add_patch(polygon)
 
     nx.draw(graph_for_reference,
             node_color=node_colormap, edge_color=edge_colormap,
@@ -750,7 +830,8 @@ initial_snapshot = BN_for_inference.predict_proba({})
 number_of_states = len(BN_for_inference.states)
 initial_precision_list = [np.nan for _ in range(len(BN_for_inference.states))]
 initial_stability_list = [np.nan for _ in range(len(BN_for_inference.states))]
-final_snapshot, precision_list, stability_list = tactical_loop(0, list(), dict(), list(), initial_snapshot, initial_precision_list, initial_stability_list)
+print()  # for aesthetics in the REPL
+final_snapshot, precision_list, stability_list = tactical_loop(0, list(), dict(), list(), initial_snapshot, initial_precision_list, initial_stability_list, skip_call_sim_heur=False)
 # final_snapshot, precision_list, stability_list = random_loop(list(), dict(), initial_snapshot, list(), list())
 report_results(final_snapshot)
 save_data_as_csv(final_snapshot)

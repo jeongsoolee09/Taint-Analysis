1a7014fd70ddb873ab24d4e1ab554cb7171c1afd
이정수 <jslee@ijeongsus-iMac.local>
2020-09-01 20:15:22 +0900

    로직 완성

--- Code/BayesianNetwork/blacklist_nodes.py
+++ Code/BayesianNetwork/blacklist_nodes.py
@@ -5,15 +5,25 @@ import glob
 # parameterizable
 PROJECT_ROOT = os.path.join('..', 'benchmarks', 'realworld', 'sagan')
 
-def create_blacklist():
-    whitelist, blacklist = [], []
+def create_whitelist_blacklist():
+    """주석"""
+    whitelist_files, blacklist_files = [], []
     for current_dir, directories, files in os.walk(PROJECT_ROOT):
         if 'tests' in current_dir or 'test' in current_dir:
-            blacklist += glob.glob(os.path.join(current_dir, "*.java"))
+            blacklist_files += glob.glob(os.path.join(current_dir, "*.java"))
         else:
-            whitelist += glob.glob(os.path.join(current_dir, "*.java"))
-    return whitelist, blacklist
+            whitelist_files += glob.glob(os.path.join(current_dir, "*.java"))
+    return whitelist_files, blacklist_files
+
+
+def extract_filename(path):
+    """path에서 (확장자를 제외한) filename만을 추출한다."""
+    _, tail = os.path.split(path)
+    return tail.split('.')[0]
 
 
 def main():
-    whitelist, blacklist = create_blacklist()
+    whitelist_files, blacklist_files = create_whitelist_blacklist()
+    whitelist = list(map(extract_filename, whitelist_files))
+    blacklist = list(map(extract_filename, blacklist_files))
+    return whitelist, blacklist

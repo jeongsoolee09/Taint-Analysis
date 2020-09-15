81b1c03a46e9a7f2d27caa1a4fc8913b6131117a
이정수 <jslee@ijeongsus-iMac.local>
2020-09-15 11:05:42 +0900

    community_detection 로직 업데이트

--- Code/BayesianNetwork/community_detection.py
+++ Code/BayesianNetwork/community_detection.py
@@ -2,8 +2,19 @@ import networkx as nx
 from networkx.algorithms import community
 
 
-def bisect(G):
-    G = G.to_undirected()
-    partition_generator = community.kernighan_lin_bisection(G)
-    partitions = list(communities_generator)
-    return partitions
+def bisect(large_graph):
+    """bisect the given graph"""
+    node_set_A, node_set_B = community.kernighan_lin_bisection(large_graph.to_undirected())
+    return recover_graph(node_set_A, large_graph), recover_graph(node_set_B, large_graph)
+
+
+def recover_graph(small_node_set, original_graph):
+    """small_node_set에서부터 small_graph를 복원해 낸다."""
+    edges = original_graph.edges
+    # 각 엣지 안에 들어 있는 노드들이 small_node_set의 원소인지를 판단하는 것으로 충분하다.
+    filterfunc = lambda edge: edge[0] in small_node_set and edge[1] in small_node_set
+    small_graph_edges = list(filter(filterfunc, edges))
+    G = nx.DiGraph()
+    G.add_nodes_from(small_node_set)
+    G.add_edges_from(small_graph_edges)
+    return G

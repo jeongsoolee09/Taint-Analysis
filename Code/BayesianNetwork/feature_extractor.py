323e3928971aa733672af9a98ad4b5d6738747d8
이정수 <jslee@ijeongsus-iMac.local>
2020-09-24 11:23:40 +0900

    feature extractor 구현 쉬운것 위주로 완료

--- Code/BayesianNetwork/feature_extractor.py
+++ Code/BayesianNetwork/feature_extractor.py
@@ -1,4 +1,4 @@
-# Feature extractor based on ISSTA'19
+# Feature extractor based on ISSTA'19: Codebase-Adaptive Detection of Security-Relevant Methods (a.k.a. SWAN)
 import modin.pandas as pd
 import re
 from functools import reduce
@@ -50,9 +50,9 @@ def find_frequent_words(**kwargs):
 # ======================================
 
 NODE_DATA = pd.read_csv("nodes.csv", index_col=0)
-TOP_FREQ_N_NAME = 10  # 상위 몇 순위까지 할 거냐?
+TOP_FREQ_N_NAME = 10            # name 낱말의 경우, 상위 몇 순위까지 고려할 거냐?
 TOP_FREQ_NAME_WORDS = list(map(lambda tup: tup[0], find_frequent_words(target="name")[:TOP_FREQ_N_NAME]))
-TOP_FREQ_N_RTNTYPE = 10
+TOP_FREQ_N_RTNTYPE = 10         # rtntype 낱말의 경우, 상위 몇 순위까지 고려할 거냐?
 TOP_FREQ_RTNTYPE_WORDS = list(map(lambda tup: tup[0],
                                   find_frequent_words(target="rtntype")[:TOP_FREQ_N_RTNTYPE]))
 
@@ -73,7 +73,7 @@ def has_return_type(node):
 
 # F14
 def method_name_starts_with(node):
-    prefix = camel_case_split(node.name)[0]
+    prefix = camel_case_split(node[3])[0]
     out = dict()
     for freq_word in TOP_FREQ_NAME_WORDS:
         out[("F14", freq_word)] = prefix == freq_word
@@ -90,7 +90,7 @@ def method_name_equals(node):
 
 # F16
 def method_name_contains(node):
-    words = camel_case_split(node.name)
+    words = camel_case_split(node[3])
     out = dict()
     for freq_word in TOP_FREQ_NAME_WORDS:
         out[("F16", freq_word)] = freq_word in words
@@ -99,37 +99,46 @@ def method_name_contains(node):
 
 # F22
 def return_type_contains_name(node):
-    words = camel_case_split(node.rtntype)
+    words = camel_case_split(node[2])
     out = dict()
-    for freq_word in TOP_FREQ_NAME_WORDS:
+    for freq_word in TOP_FREQ_RTNTYPE_WORDS:
         out[("F22", freq_word)] = freq_word in words
     return out    # key: word, val: boolean
 
 # Batch run ==========================
 # ====================================
 
-def run_all_extractors(node):
+def run_all_extractors(node_row):
     """batch run the feature extractors on a method"""
-    F06 = has_parameters(node)  # bool: 바로 append
-    F07 = has_return_type(node)  # bool: 바로 append
-    F14 = method_name_starts_with(node)  # dict: pd.dataframe
-    F15 = method_name_equals(node)       # dict: pd.dataframe
-    F16 = method_name_contains(node)     # dict: pd.dataframe
-    F22 = return_type_contain_name(node)  # dict: pd.dataframe
-    
+    F06 = has_parameters(node_row)  # bool
+    F06_df = pd.DataFrame([F06], columns=pd.MultiIndex.from_tuples([("F06", "")]))
+
+    F07 = has_return_type(node_row)  # bool
+    F07_df = pd.DataFrame([F07], columns=pd.MultiIndex.from_tuples([("F07", "")]))
+
+    F14 = method_name_starts_with(node_row)  # dict
+    F14_df = pd.DataFrame(F14, index=[0])
+
+    F15 = method_name_equals(node_row)       # dict
     F15_df = pd.DataFrame(F15, index=[0])
+
+    F16 = method_name_contains(node_row)     # dict
     F16_df = pd.DataFrame(F16, index=[0])
+
+    F22 = return_type_contains_name(node_row)  # dict
     F22_df = pd.DataFrame(F22, index=[0])
     
-    vector = pd.concat([F15_df, F16_df, F22_df], axis=1)
+    vector = pd.concat([F06_df, F07_df, F15_df, F16_df, F22_df], axis=1)
 
     return vector
 
 # main ================================
 # =====================================
 
-
 def main():
-    # batch run the feature extractors on all methods (or maybe we can use modin.pandas.apply)
-    # and then write to csv
-    pass
+    sim_df = pd.DataFrame()
+    for tup in NODE_DATA.itertuples(index=False):
+        vector = run_all_extractors(tup)
+        sim_df = pd.concat([sim_df, vector])
+    sim_df.to_csv("sim_vectors.csv", mode="w+")
+

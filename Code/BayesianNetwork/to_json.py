0c2ded5d1f44a85e38a8cbb9cdb8aba2537206d2
jeongsoolee09 <volcano0909@gmail.com>
2021-03-03 15:00:00 +0900

    delete old unused bench

--- Code/BayesianNetwork/to_json.py
+++
@@ -1,9 +0,0 @@
-from solutions import *
-import json
-
-with open("solution_sagan.json", "w+") as sagan:
-    json.dump(correct_solution_sagan, sagan, indent=2)
-with open("solution_WhatIWantExample.json", "w+") as wwe:
-    json.dump(correct_solution_WhatIWantExample, wwe, indent=2)
-with open("solution_relational.json", "w+") as relational:
-    json.dump(correct_solution_relational, relational, indent=2)

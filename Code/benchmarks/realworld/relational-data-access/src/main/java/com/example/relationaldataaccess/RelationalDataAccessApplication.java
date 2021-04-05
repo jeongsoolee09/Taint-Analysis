6194facde63a83237f38ad354fafdc79e12a4d1d
jeongsoolee09 <volcano0909@gmail.com>
2021-04-05 19:37:50 +0900

    diet more

--- Code/benchmarks/realworld/relational-data-access/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
+++ Code/benchmarks/realworld/relational-data-access/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
@@ -1,56 +1,45 @@
 package com.example.relationaldataaccess;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.Scanner;
 import java.util.Map;
 
 @SpringBootApplication
-public class RelationalDataAccessApplication implements CommandLineRunner {
+public class RelationalDataAccessApplication {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-    private static final Logger log = LoggerFactory.getLogger(RelationalDataAccessApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RelationalDataAccessApplication.class, args);
     }
 
-    public String[] create() {
+    public String create() {
         Scanner scanner = new Scanner(System.in);
-        String firstName = scanner.nextLine();
-        String lastName = scanner.nextLine();
-        String[] out = new String[2];
-        out[0] = firstName;
-        out[1] = lastName;
+        String name = scanner.nextLine();
 
-        return out;
+        return name;
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-        String[] splitUpNames = create();
-        String firstName = splitUpNames[0];
-        String lastName = splitUpNames[1];
-        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES ("+firstName+","+lastName+")");
+    public void run() throws Exception {
+        String name = create();
+        String sql = "INSERT INTO customers VALUES (" + name + ")";
+        jdbcTemplate.batchUpdate(sql);
     }
 
     public Map<String, Object> query() throws Exception {
-        Map<String, Object> results = jdbcTemplate.queryForMap("SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { });
+        Map<String, Object> results = jdbcTemplate.queryForMap("SELECT id, first_name, FROM customers WHERE first_name = ?", new Object[] { "John" });
         return results;
     }
 
     public void printer(Map<String, Object> results) throws Exception {
-        for (Object name : results.values())
-            System.out.println("name: " + name);
+        
+       Object elem = results.get("John");
+       System.out.println(elem);
     }
 }

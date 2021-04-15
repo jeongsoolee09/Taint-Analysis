3e4dc0c04c24bb294c5f04d9167b0f5deb5797f3
jeongsoolee09 <volcano0909@gmail.com>
2021-04-15 18:53:09 +0900

    shell scripts

---
+++ Code/BayesianNetwork/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Automates cleaning up the directory between entire interaction sessions
+
+echo "cleaning up directory..."
+
+rm *.csv
+rm callgraph
+rm graph_for_reference
--- Code/BayesianNetwork/main.sh
+++ Code/BayesianNetwork/main.sh
@@ -2,19 +2,37 @@
 
 # Automates the process right before running loop or self_question_n_answer
 
-echo "creating nodes.."
+echo "creating CPT_lookup_table..."
+python make_CPT_lookup_table.py
+echo "\n"
+
+echo "creating nodes..."
 python create_node.py
+echo "\n"
 
-echo "extracting features.."
-python feature_extract.py
-
-echo "creating edges.."
+echo "creating df and call edges..."
 python create_edge.py
+echo "\n"
 
-echo "making underlying graph.."
+echo "computing pairwise similarity..."
+python pairwise_similarity.py
+echo "\n"
+
+echo "extracting extra features..."
+python extra_features.py
+echo "\n"
+
+
+echo "making underlying graph..."
 python make_underlying_graph.py
+echo "\n"
 
-echo "splitting underlying graph.."
+echo "splitting underlying graph..."
 python split_underlying_graph.py
+echo "\n"
+
+echo "reconnecting possibly missing edges..."
+python reconnect_poor_nodes.py
+echo "\n"
 
 echo "Done! Now either run loop.py or self_question_n_answer.py."

0aa6663ab53c9b744e5a7ddab6345f852a1104d2
jeongsoolee09 <volcano0909@gmail.com>
2021-04-22 17:56:03 +0900

    print_path 확장하기 전

--- Code/BayesianNetwork/hack_the_structure.py
+++ Code/BayesianNetwork/hack_the_structure.py
@@ -1,13 +1,28 @@
 import graphviz
 import networkx as nx
+import networkx.algorithms as nxalg
+import networkx.algorithms.traversal.depth_first_search as dfs
 import make_BN
 import os
 import re
 import numpy as np
+import json
 
 from functools import reduce
 
 graph_files = [f for f in os.listdir('.') if re.match(r'.*_graph_[0-9]+$', f)]
+
+def retrieve_path():
+    """paths.json을 읽고 path를 가져온다."""
+    with open("paths.json", "r+") as pathjson:
+        pathdict = json.load(pathjson)
+    return pathdict["project_root_directory"]
+
+PROJECT_ROOT_DIR = retrieve_path()
+
+with open(os.path.join(PROJECT_ROOT_DIR, "skip_func.txt"), "r+") as skip_func:
+    skip_funcs = skip_func.readlines()
+    skip_funcs = list(map(lambda string: string.rstrip(), skip_funcs))
 
 
 def depickle_all():
@@ -44,6 +59,34 @@ def partition(coll, n):
     return acc
 
 
+def print_path(graph_for_reference, from_node):
+    """BN 상에서 from_node로부터 to_node까지, update가 일어난 path들을 모두 pretty print."""
+    # assert len(rich_nodes(graph_for_reference)) == 0  # 야호! 통과한다. 하지만 혹시 몰라 남겨 놓음
+
+    def inner(path, graph_for_reference):
+        """ [1, 2, 4] |-> \"1 --call--> 2 --DF--> 4\" """
+        zipped = list(zip(path[:len(path)-1], path[1:]))
+
+        def mapfunc(edge):
+            try:
+                edgekind_dict = graph_for_reference.get_edge_data(edge[0], edge[1])
+                edgekind = edgekind_dict["kind"] # may throw TypeError because edgekind_dict may be None
+                return str(edge[0]) + "--" + edgekind + "-->" + str(edge[1])
+            except TypeError:
+                edgekind_dict = graph_for_reference.get_edge_data(edge[1], edge[0])
+                edgekind = edgekind_dict["kind"]
+                return str(edge[0]) + "<--" + edgekind + "--" + str(edge[1])
+
+        mapped = list(map(mapfunc, zipped))
+        return mapped
+
+    BN_undirected = graph_for_reference.to_undirected()
+    paths = list(dfs.dfs_preorder_nodes(BN_undirected, from_node))
+    i = 1
+    for path in paths:
+        print("path"+str(i)+":", inner(path, graph_for_reference))
+        i += 1
+
 def make_bicycle_chain(coll):
     all_bust_last = coll[:len(coll)-1]
     all_bust_first = coll[1:]
@@ -73,13 +116,19 @@ def transpose(ndlist):
     return np.array(ndlist).T.tolist()
 
 
-def make_grid_layout(nx_graph):
+def make_grid_layout(nx_graph, with_edge=True):
     """networkx graph를 받아서, GraphViz dot 포맷을 빌드한다."""
     graph = graphviz.Digraph()
 
     list_of_nodes = list(nx_graph.nodes)
     partitioned = partition(list_of_nodes, 5)  # 5 * n 사이즈로 만들어 봅시다!
     partitioned_T = transpose(partitioned)
+
+    for node_name in list_of_nodes:
+        if node_name in skip_funcs:
+            graph.node(node_name, style="filled", fillcolor="purple")
+        else:
+            graph.node(node_name)
 
     for part in partitioned_T:
         bicycle_chain = make_bicycle_chain(part)
@@ -92,20 +141,41 @@ def make_grid_layout(nx_graph):
 
     list_of_edges = list(nx_graph.edges)
 
-    return graph, graph.source
+    if with_edge:
+        for edge in nx_graph.edges:
+            edgekind = nx_graph.get_edge_data(*edge)["kind"]
+            if edgekind == "df":
+                graph.edge(edge[0], edge[1], color="red")
+            elif edgekind == "call":
+                graph.edge(edge[0], edge[1], color="black")
+            elif edgekind == "sim":
+                graph.edge(edge[0], edge[1], color="blue")
+            else:
+                raise ValueError(f"invalid edge data: {edgekind}")
+
+    return graph
 
 
 def main():
     graph0 = nx.read_gpickle("Decision-1.1.0_graph_0")
     make_BN.tame_rich(graph0)   # to simulate the making of BN
-    dot_graph, sourcecode = make_grid_layout(graph0)
-    with open("graph0.dot", "w+") as f:
-        f.write(sourcecode)
-    dot_graph.render(filename="graph0",
-                     format="pdf",
-                     view=False,
-                     quiet=True,
-                     cleanup=True)
+
+    # without edges
+    dot_graph_without_edge = make_grid_layout(graph0, with_edge=False)
+    dot_graph_without_edge.render(filename="graph0_without_edge",
+                                  format="pdf",
+                                  view=False,
+                                  quiet=True,
+                                  cleanup=True)
+
+    # with colored edges
+    dot_graph_with_edge = make_grid_layout(graph0, with_edge=True)
+    dot_graph_with_edge.render(filename="graph0_with_edge",
+                               format="pdf",
+                               view=False,
+                               quiet=True,
+                               cleanup=True)
+
     print("done!")
 
 

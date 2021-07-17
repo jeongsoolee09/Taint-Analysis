d6b35b857efc1254ca6d394cff26a62479335682
jeongsoolee09 <volcano0909@gmail.com>
2021-07-17 17:17:47 +0900

    delete trailing whitespaces && rename inner functions

--- Code/infer/infer/src/checkers/defLocAliasSearches.ml
+++ Code/infer/infer/src/checkers/defLocAliasSearches.ml
@@ -19,72 +19,72 @@ let placeholder_vardef (pid:Procname.t) : Var.t =
 
 let search_target_tuple_by_pvar (pvar:Var.t) (methname:Procname.t) (astate_set:S.t) : T.t =
   let elements = S.elements astate_set in
-  let rec search_target_tuple_by_pvar_inner pvar (methname:Procname.t) elements = 
+  let rec inner pvar (methname:Procname.t) elements =
     match elements with
     | [] -> L.die InternalError "search_target_tuple_by_pvar failed, pvar: %a, methname:%a, astate_set:%a@." Var.pp pvar Procname.pp methname S.pp astate_set
     | ((procname, _, _, aliasset) as target)::t ->
-        if Procname.equal procname methname && A.mem (pvar, []) aliasset then target else search_target_tuple_by_pvar_inner pvar methname t in
-  search_target_tuple_by_pvar_inner pvar methname elements
+        if Procname.equal procname methname && A.mem (pvar, []) aliasset then target else inner pvar methname t in
+  inner pvar methname elements
 
 
 (* list version of the above *)
 let search_target_tuples_by_pvar (pvar:Var.t) (methname:Procname.t) (tupleset:S.t) : T.t list =
   let elements = S.elements tupleset in
-  let rec search_target_tuples_by_pvar_inner pvar (methname:Procname.t) elements = 
+  let rec inner pvar (methname:Procname.t) elements =
     match elements with
     | [] -> []
     | ((procname, _, _, aliasset) as target)::t ->
         if Procname.equal procname methname && A.mem (pvar, []) aliasset
-        then target::search_target_tuples_by_pvar_inner pvar methname t
-        else search_target_tuples_by_pvar_inner pvar methname t in
-  search_target_tuples_by_pvar_inner pvar methname elements
+        then target::inner pvar methname t
+        else inner pvar methname t in
+  inner pvar methname elements
 
 
 let search_target_tuple_by_id (id:Ident.t) (methname:Procname.t) (astate_set:S.t) : T.t =
   (* L.d_printfln "id: %a, methname: %a, tupleset: %a@." Ident.pp id Procname.pp methname S.pp tupleset; *)
   let elements = S.elements astate_set in
-  let rec search_target_tuple_by_id_inner id (methname:Procname.t) elements = 
+  let rec inner id (methname:Procname.t) elements =
     match elements with
     | [] -> (* L.die InternalError "search_target_tuple_by_id failed, id: %a, methname: %a, tupleset: %a@." Ident.pp id Procname.pp methname S.pp astate_set *)
       raise IDontKnow
     | ((procname, _, _, aliasset) as target)::t ->
-        if Procname.equal procname methname && A.mem (Var.of_id id, []) aliasset then target else search_target_tuple_by_id_inner id methname t in
-  search_target_tuple_by_id_inner id methname elements
+        if Procname.equal procname methname && A.mem (Var.of_id id, []) aliasset then target else inner id methname t in
+  inner id methname elements
 
 
 let search_target_tuple_by_pvar_ap (ap:MyAccessPath.t) (methname:Procname.t) (astate_set:S.t) : T.t =
   let elements = S.elements astate_set in
-  let rec search_target_tuple_by_pvar_inner pvar (methname:Procname.t) elements = 
+  let rec inner pvar (methname:Procname.t) elements =
     match elements with
     | [] -> L.die InternalError "search_target_tuple_by_pvar_ap failed, ap: %a, methname:%a, astate_set:%a@." MyAccessPath.pp pvar Procname.pp methname S.pp astate_set
     | ((procname, _, _, aliasset) as target)::t ->
-        if Procname.equal procname methname && A.mem ap aliasset then target else search_target_tuple_by_pvar_inner pvar methname t in
-  search_target_tuple_by_pvar_inner ap methname elements
+        if Procname.equal procname methname && A.mem ap aliasset then target else inner pvar methname t in
+  inner ap methname elements
 
 
 let search_target_tuples_by_id (id:Ident.t) (methname:Procname.t) (astate_set:S.t) : T.t list =
   let elements = S.elements astate_set in
-  let rec search_target_tuples_by_id_inner id (methname:Procname.t) elements acc = 
+  let rec inner id (methname:Procname.t) elements acc =
     match elements with
     | [] -> acc
     | ((procname, _, _, aliasset) as target)::t ->
         if Procname.equal procname methname && A.mem (Var.of_id id, []) aliasset
-        then search_target_tuples_by_id_inner id methname t (target::acc)
-        else search_target_tuples_by_id_inner id methname t acc in
-  search_target_tuples_by_id_inner id methname elements []
+        then inner id methname t (target::acc)
+        else inner id methname t acc in
+  inner id methname elements []
 
 
 let weak_search_target_tuple_by_id (id:Ident.t) (astate_set:S.t) : T.t =
   let elements = S.elements astate_set in
-  let rec weak_search_target_tuple_by_id_inner id (elements:S.elt list) =
+  let rec inner id (elements:S.elt list) =
     match elements with
     | [] -> L.die InternalError "weak_search_target_tuple_by_id failed, id: %a, astate_set: %a@." Ident.pp id S.pp astate_set
     | target::t ->
         let aliasset = fourth_of target in
         if A.mem (Var.of_id id, []) aliasset
         then target
-        else weak_search_target_tuple_by_id_inner id t in
-  weak_search_target_tuple_by_id_inner id elements
+        else inner id t in
+  inner id elements
 
 
 let is_return_ap (ap:A.elt) : bool =
@@ -94,38 +94,38 @@ let is_return_ap (ap:A.elt) : bool =
 
 let find_tuples_with_ret (tupleset:S.t) (methname:Procname.t) : T.t list =
   let elements = S.elements tupleset in
-  let rec find_tuple_with_ret_inner (tuplelist:T.t list) (methname:Procname.t) (acc:T.t list) =
+  let rec inner (tuplelist:T.t list) (methname:Procname.t) (acc:T.t list) =
     match tuplelist with
     | [] -> acc
     | (procname, _, _, aliasset) as target :: t ->
         if Procname.equal procname methname && A.exists is_return_ap aliasset
-        then find_tuple_with_ret_inner t methname (target::acc)
-        else find_tuple_with_ret_inner t methname acc in
-  find_tuple_with_ret_inner elements methname []
+        then inner t methname (target::acc)
+        else inner t methname acc in
+  inner elements methname []
 
 
 let search_target_tuples_by_vardef (pv:Var.t) (methname:Procname.t) (tupleset:S.t) : T.t list =
   let elements = S.elements tupleset in
-  let rec search_target_tuples_by_vardef_inner pv (methname:Procname.t) elements acc = 
+  let rec inner pv (methname:Procname.t) elements acc =
     match elements with
     | [] -> acc
     | ((procname, (vardef, _), _, _) as target) :: t ->
         if Procname.equal procname methname && Var.equal vardef pv
-        then search_target_tuples_by_vardef_inner pv methname t (target::acc)
-        else search_target_tuples_by_vardef_inner pv methname t acc in
-  search_target_tuples_by_vardef_inner pv methname elements []
+        then inner pv methname t (target::acc)
+        else inner pv methname t acc in
+  inner pv methname elements []
 
 
 let search_target_tuples_by_vardef_ap (pv_ap:MyAccessPath.t) (methname:Procname.t) (tupleset:S.t) : T.t list =
   let elements = S.elements tupleset in
-  let rec search_target_tuples_by_vardef_ap_inner pv_ap methname elements acc =
+  let rec inner pv_ap methname elements acc =
     match elements with
     | [] -> acc
     | ((procname, var_ap, _, _) as target) :: t ->
         if Procname.equal procname methname && MyAccessPath.equal pv_ap var_ap
-        then search_target_tuples_by_vardef_ap_inner pv_ap methname t (target::acc)
-        else search_target_tuples_by_vardef_ap_inner pv_ap methname t acc in
-  search_target_tuples_by_vardef_ap_inner pv_ap methname elements []
+        then inner pv_ap methname t (target::acc)
+        else inner pv_ap methname t acc in
+  inner pv_ap methname elements []
 
 
 let pp_tuplelist fmt (tuplelist:T.t list) : unit =
@@ -162,24 +162,24 @@ let rec search_tuples_by_loc (loc_set:LocationSet.t) (tuplelist:S.elt list) : T.
       else search_tuples_by_loc loc_set t
 
 
-  (* x => y: y is more recent than x in a same file *)
-  let (=>) (x:LocationSet.t) (y:LocationSet.t) : bool =
-    let x_min = LocationSet.min_elt x in
-    let y_min = LocationSet.min_elt y in
-    let loc_cond = x_min.line <= y_min.line in
-    SourceFile.equal x_min.file y_min.file && loc_cond
+(* x => y: y is more recent than x in a same file *)
+let (=>) (x:LocationSet.t) (y:LocationSet.t) : bool =
+  let x_min = LocationSet.min_elt x in
+  let y_min = LocationSet.min_elt y in
+  let loc_cond = x_min.line <= y_min.line in
+  SourceFile.equal x_min.file y_min.file && loc_cond
 
 
-    (* x ==> y: y is STRICTLY more recent than x in a same file *)
-  let (==>) (x:LocationSet.t) (y:LocationSet.t) : bool =
-    let x_min = LocationSet.min_elt x in
-    let y_min = LocationSet.min_elt y in
-    let loc_cond = x_min.line < y_min.line in
-    SourceFile.equal x_min.file y_min.file && loc_cond
+  (* x ==> y: y is STRICTLY more recent than x in a same file *)
+let (==>) (x:LocationSet.t) (y:LocationSet.t) : bool =
+  let x_min = LocationSet.min_elt x in
+  let y_min = LocationSet.min_elt y in
+  let loc_cond = x_min.line < y_min.line in
+  SourceFile.equal x_min.file y_min.file && loc_cond
 
 
 let find_least_linenumber (statelist:T.t list) : T.t =
-  let rec find_least_linenumber_inner (statelist:T.t list) (current_least:T.t) : T.t =
+  let rec inner (statelist:T.t list) (current_least:T.t) : T.t =
     L.d_printfln "statelist: %a@." pp_tuplelist statelist;
     match statelist with
     | [] -> current_least
@@ -187,9 +187,9 @@ let find_least_linenumber (statelist:T.t list) : T.t =
         let snd_target = third_of targetState in
         let snd_current = third_of current_least in
         if snd_target ==> snd_current
-        then find_least_linenumber_inner t targetState
-        else find_least_linenumber_inner t current_least in
-  find_least_linenumber_inner statelist (List.nth_exn statelist 0)
+        then inner t targetState
+        else inner t current_least in
+  inner statelist (List.nth_exn statelist 0)
 
 
 (** pick the earliest TUPLE within a list of astates *)
@@ -250,11 +250,11 @@ let is_program_var (var:Var.t) : bool =
 (** Given a alias set, finds the tuple with a Pvar in it. *)
 let find_another_pvar_vardef (varset:A.t) : A.elt =
   let varlist = A.elements varset in
-  let rec find_another_pvar_vardef_inner (varlist:A.elt list) =
+  let rec inner (varlist:A.elt list) =
     match varlist with
     | [] -> L.die InternalError "find_another_pvar_vardef failed, varset: %a@." A.pp varset
-    | (var, _) as target::t -> if is_program_var var then target else find_another_pvar_vardef_inner t in
-  find_another_pvar_vardef_inner varlist
+    | (var, _) as target::t -> if is_program_var var then target else inner t in
+  inner varlist
 
 
 let extract_from_singleton (singleton:A.t) : A.elt =
@@ -267,12 +267,12 @@ let extract_from_singleton (singleton:A.t) : A.elt =
 (** A.t의 aliasset 안에서 pvar 튜플을 찾아낸다. *)
 let find_pvar_ap_in (aliasset:A.t) : A.elt =
   let elements = A.elements aliasset in
-  let rec find_pvar_ap_in_inner (elements:A.elt list) (acc:A.elt list) =
+  let rec inner (elements:A.elt list) (acc:A.elt list) =
   match elements with
   | [] -> acc
   | (var, _) as target::t ->
-      if is_program_var var then target::acc else find_pvar_ap_in_inner t acc in
-  let result = find_pvar_ap_in_inner elements [] in
+      if is_program_var var then target::acc else inner t acc in
+  let result = inner elements [] in
   begin match result with
     | [] -> L.die InternalError "find_pvar_ap_in failed (too few), aliasset: %a@." A.pp aliasset
     | [x] -> x
@@ -282,23 +282,23 @@ let find_pvar_ap_in (aliasset:A.t) : A.elt =
 (** aliasset이 주어졌을 때 그 중에서 field를 갖고 있는 튜플을 내놓는다. *)
 let find_ap_with_field (aliasset:A.t) : A.elt =
   let elements = A.elements aliasset in
-    let rec find_ap_with_field_inner (elements:A.elt list) =
+    let rec inner (elements:A.elt list) =
     match elements with
     | [] -> L.die InternalError "find_ap_with_field failed, aliasset: %a@." A.pp aliasset
-    | (_, aplst) as target :: t -> if List.length aplst > 0 then target else find_ap_with_field_inner t in
-  find_ap_with_field_inner elements
+    | (_, aplst) as target :: t -> if List.length aplst > 0 then target else inner t in
+  inner elements
 
 
 let search_target_tuple_by_vardef_ap (ap:MyAccessPath.t) (methname:Procname.t) (astate_set:S.t) : T.t =
   let elements = S.elements astate_set in
-  let rec search_target_tuple_by_ap_inner (ap:MyAccessPath.t) (methname:Procname.t) (elements:S.elt list) = 
+  let rec inner (ap:MyAccessPath.t) (methname:Procname.t) (elements:S.elt list) =
     match elements with
     | [] -> raise IDontKnow (* L.die InternalError "search_target_tuple_by_vardef_ap failed, ap:%a, methname: %a, elements: %a" MyAccessPath.pp ap Procname.pp methname pp_tuplelist elements *)
     | ((procname, vardef, _, _) as target)::t ->
         if Procname.equal procname methname && MyAccessPath.equal ap vardef
         then target
-        else search_target_tuple_by_ap_inner ap methname t in
-  search_target_tuple_by_ap_inner ap methname elements
+        else inner ap methname t in
+  inner ap methname elements
 
 
 let search_astate_holding_param (astate_set: S.t) (param: A.elt) : S.elt =

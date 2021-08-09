e37a4a9eab14828430f703095dac16f37746ff57
jeongsoolee09 <volcano0909@gmail.com>
2021-08-09 18:41:29 +0900

    update partitioners

--- Code/infer/infer/src/spechunter/RefineSummaries.ml
+++ Code/infer/infer/src/spechunter/RefineSummaries.ml
@@ -15,14 +15,18 @@ module F = Format
 
 (* Exceptions ======================================= *)
 (* ================================================== *)
+
 exception TODO
 
 exception NotASingleton of string
 
 exception TooManyMatches of string
 
-let partition_statetups_by_procname (statetups : S.t) : (Procname.t * S.t) list =
-  let partitions =
+(* Partitioners ===================================== *)
+(* ================================================== *)
+
+let partition_statetups_by_procname (statetups : S.t) : S.t list =
+  let procnames =
     List.stable_dedup
     @@ S.fold
          (fun astate acc ->
@@ -38,12 +42,12 @@ let partition_statetups_by_procname (statetups : S.t) : (Procname.t * S.t) list 
             if Procname.equal procname (first_of statetup) then S.add statetup acc' else acc')
           statetups S.empty
       in
-      (procname, matches) :: acc)
-    ~init:[] partitions
+      matches :: acc)
+    ~init:[] procnames
 
 
-let partition_statetups_by_vardef (statetups : S.t) : (MyAccessPath.t * S.t) list =
-  let partitions =
+let partition_statetups_by_vardef (statetups : S.t) : S.t list =
+  let vardefs =
     List.stable_dedup
     @@ S.fold
          (fun astate acc ->
@@ -59,11 +63,11 @@ let partition_statetups_by_vardef (statetups : S.t) : (MyAccessPath.t * S.t) lis
             if MyAccessPath.equal vardef (second_of statetup) then S.add statetup acc' else acc')
           statetups S.empty
       in
-      (vardef, matches) :: acc)
-    ~init:[] partitions
+      matches :: acc)
+    ~init:[] vardefs
 
 
-let partition_statetups_by_locset (statetups : S.t) : (LocationSet.t * S.t) list =
+let partition_statetups_by_locset (statetups : S.t) : S.t list =
   let locsets : LocationSet.t list =
     List.stable_dedup
     @@ S.fold
@@ -80,12 +84,12 @@ let partition_statetups_by_locset (statetups : S.t) : (LocationSet.t * S.t) list
             if LocationSet.equal locset (third_of statetup) then S.add statetup acc' else acc')
           statetups S.empty
       in
-      (locset, matches) :: acc)
+      matches :: acc)
     ~init:[] locsets
 
 
-let partition_statetups_by_aliasset (statetups : S.t) : (A.t * S.t) list =
-  let partitions =
+let partition_statetups_by_aliasset (statetups : S.t) : S.t list =
+  let locsets =
     List.stable_dedup
     @@ S.fold
          (fun astate acc ->
@@ -101,8 +105,43 @@ let partition_statetups_by_aliasset (statetups : S.t) : (A.t * S.t) list =
             if A.equal locset (fourth_of statetup) then S.add statetup acc' else acc')
           statetups S.empty
       in
-      (locset, matches) :: acc)
-    ~init:[] partitions
+      matches :: acc)
+    ~init:[] locsets
+
+
+let partition_statetups_by_vardef_and_locset (statetups : S.t) : S.t list =
+  let vardefs =
+    List.stable_dedup
+    @@ S.fold
+         (fun astate acc ->
+           let vardef = second_of astate in
+           vardef :: acc)
+         statetups []
+  in
+  let locsets : LocationSet.t list =
+    List.stable_dedup
+    @@ S.fold
+         (fun astate acc ->
+           let locset = third_of astate in
+           locset :: acc)
+         statetups []
+  in
+  let vardef_locset_pairs =
+    let open List in
+    vardefs >>= fun vardef -> locsets >>= fun locset -> return (vardef, locset)
+  in
+  List.fold
+    ~f:(fun acc (vardef, locset) ->
+      let matches =
+        S.fold
+          (fun ((_, vardef', locset', _) as statetup) acc' ->
+            if MyAccessPath.equal vardef vardef' && LocationSet.equal locset locset' then
+              S.add statetup acc'
+            else acc')
+          statetups S.empty
+      in
+      if S.is_empty matches then acc else matches :: acc)
+    ~init:[] vardef_locset_pairs
 
 
 (** Return the first matching value for a key in a association list. *)

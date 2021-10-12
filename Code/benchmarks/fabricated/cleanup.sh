04e99716a974b800e84834f0d46589d46586b6c5
jeongsoolee09 <volcano0909@gmail.com>
2021-10-12 15:19:46 +0900

    minor

--- Code/benchmarks/fabricated/cleanup.sh
+++ Code/benchmarks/fabricated/cleanup.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-rm -rf *.dot || rm -rf infer-out/ || rm -rf  *.class || rm -rf  *.txt || rm -rf *.json || rm -rf *.png
+rm -rf *.dot
+rm -rf infer-out/
+rm -rf *.class
+rm -rf *.txt
+rm -rf *.json
+rm -rf *.png
+rm -rf *.svg

bbd809961a761d16e3452eabd608a227cb9a9004
jeongsoolee09 <volcano0909@gmail.com>
2021-10-13 13:29:34 +0900

    update SpecHunterUtils

--- Code/infer/infer/src/spechunter/SpecHunterUtils.ml
+++ Code/infer/infer/src/spechunter/SpecHunterUtils.ml
@@ -5,6 +5,8 @@ open! IStd
 module L = Logging
 module F = Format
 
+exception FindIndexFailed
+
 let option_get : 'a option -> 'a = function
   | None ->
       L.die InternalError "Given option is empty"
@@ -20,3 +22,14 @@ let rec catMaybes_tuplist (optlist : ('a * 'b option) list) : ('a * 'b) list =
       (sth1, sth2) :: catMaybes_tuplist t
   | (_, None) :: t ->
       catMaybes_tuplist t
+
+
+let find_index_of (elem : 'a) (lst : 'a list) ~(equal : 'a -> 'a -> bool) : int =
+  let rec inner (acc : int) (lst : 'a list) =
+    match lst with
+    | [] ->
+        raise FindIndexFailed
+    | h :: t ->
+        if equal h elem then acc else inner (acc + 1) t
+  in
+  inner 0 lst

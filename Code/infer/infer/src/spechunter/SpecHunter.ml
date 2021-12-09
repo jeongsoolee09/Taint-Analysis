339e2d4cada211a2f2b346b6bdc1a92c3ead2670
jeongsoolee09 <volcano0909@gmail.com>
2021-12-09 13:42:38 +0900

    compute things only if necessary

--- Code/infer/infer/src/spechunter/SpecHunter.ml
+++ Code/infer/infer/src/spechunter/SpecHunter.ml
@@ -5,9 +5,25 @@ module D = DefLocAlias
 module Hashtbl = Caml.Hashtbl
 module TestMap = PrettyPrintable.MakePPMap (Int)
 
+
+let run_only_when_not_file_exists (f: unit -> unit) (filename: string) : unit =
+    match Sys.file_exists (F.asprintf "%s/%s" (Sys.getcwd ()) filename) with
+    | `No | `Unknown -> f ()
+    | `Yes -> L.progress "Skipped calculating %s because it's already there.@." filename
+
+
+let infer_analyze_main () =
+    InferAnalyze.main ~changed_files:None;
+    let hashtbl = Hashtbl.create 777 in
+    SummaryLoader.load_summary_from_disk_to hashtbl ~exclude_test:true;
+    let out_chan = Out_channel.create "AnalysisResult.bin" in
+    Marshal.to_channel out_chan hashtbl [];
+    Out_channel.close out_chan
+
+
 let main () =
-  InferAnalyze.main ~changed_files:None ;
-  SetofAllMeths.calculate () ;
-  ExtractAnnotations.main () ;
-  LiveRange.main () ;
-  GetterSetter.main ()
+  run_only_when_not_file_exists infer_analyze_main "AnalysisResult.bin";
+  run_only_when_not_file_exists SetofAllMeths.calculate "Methods.txt";
+  run_only_when_not_file_exists ExtractAnnotations.main "Annotations.json";
+  run_only_when_not_file_exists LiveRange.main "Chain.json";
+  run_only_when_not_file_exists GetterSetter.main  "GetterSetter.json"

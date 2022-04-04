22f7f3e50cd8399cfdf49d4a115dbb350f6b4547
이정수 <jeongsoolee@korea.ac.kr>
2022-04-04 21:27:28 +0900

    add TestCodeDetector

---
+++ Code/infer/infer/src/spechunter/TestCodeDetector.ml
@@ -0,0 +1,77 @@
+open DefLocAliasDomain
+module F = Format
+module Hashtbl = Caml.Hashtbl
+
+let ( >>= ) = List.( >>= )
+
+and ( >>| ) = List.( >>| )
+
+let is_directory (abs_dir : string) : bool =
+  match Sys.is_directory abs_dir with `Yes -> true | _ -> false
+
+
+let walk_for_extension (root_dir : string) (extension : string) : string list =
+  let rec inner (current_dir : string) (filename_acc : string list) =
+    let subdirectories =
+      Array.filter ~f:is_directory
+        (Array.map ~f:(fun name -> current_dir ^ "/" ^ name) (Sys.readdir current_dir))
+    in
+    let files_matching_extension =
+      Array.filter ~f:(not << is_directory)
+        (Array.map ~f:(fun name -> current_dir ^ "/" ^ name) (Sys.readdir current_dir))
+      |> Array.fold
+           ~f:(fun acc elem -> if String.is_suffix elem ~suffix:extension then elem :: acc else acc)
+           ~init:[]
+    in
+    if Array.is_empty subdirectories then filename_acc @ files_matching_extension
+    else
+      Array.fold subdirectories
+        ~f:(fun acc subdirectory -> inner subdirectory acc)
+        ~init:(filename_acc @ files_matching_extension)
+  in
+  inner root_dir []
+
+
+let extract_filename_without_extension_and_dirs (full_filename : string) : string =
+  let filename_only = List.last_exn @@ String.split ~on:'/' full_filename in
+  List.hd_exn @@ String.split ~on:'.' filename_only
+
+
+(** does this directory have any java file (recursively?) *)
+let has_java (dir : string) : bool = not @@ List.is_empty @@ walk_for_extension dir ".java"
+
+(** get the subdirs containing .java files. *)
+let get_compilation_unit_absdirs (root_dir : string) : string list =
+  Sys.readdir root_dir
+  |> Array.map ~f:(fun subdir -> root_dir ^ subdir)
+  |> Array.filter ~f:is_directory |> Array.filter ~f:has_java |> Array.to_list
+
+
+let get_compilation_unit_subdirs (root_dir : string) : string list =
+  let absdirs = get_compilation_unit_absdirs root_dir in
+  List.map ~f:(fun absdir -> List.last_exn @@ String.split ~on:'/' absdir) absdirs
+
+
+let get_test_classnames =
+  let cache = Hashtbl.create 777 in
+  fun (root_dir : string) : string list ->
+    match Hashtbl.find_opt cache root_dir with
+    | None ->
+        let out =
+          walk_for_extension root_dir ".java"
+          |> List.filter ~f:(String.is_substring ~substring:"/test/")
+          >>| extract_filename_without_extension_and_dirs
+        in
+        Hashtbl.add cache root_dir out ;
+        out
+    | Some res ->
+        res
+
+
+let is_test_method (proc : Procname.t) : bool =
+  let classname =
+    List.last_exn @@ String.split ~on:'.' @@ Option.value ~default:""
+    @@ Procname.get_class_name proc
+  in
+  let test_classnames = get_test_classnames @@ Sys.getcwd () in
+  List.mem test_classnames classname ~equal:String.equal

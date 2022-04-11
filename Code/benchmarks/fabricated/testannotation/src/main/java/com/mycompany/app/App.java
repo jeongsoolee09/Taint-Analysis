e18bab19d42784d4176f0a2f29f179af48dbd4fc
이정수 <jeongsoolee@korea.ac.kr>
2022-04-11 18:15:50 +0900

    update

---
+++ Code/benchmarks/fabricated/testannotation/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,22 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    @Test
+    public int shouldAnswerWithTrue() {
+        int x = 1;
+        return x;
+    }
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
